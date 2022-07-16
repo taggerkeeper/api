14c41429151064a037fde136dba474f78872bb0d
Jason Godesky <jefgodesky@gmail.com>
2022-07-16 15:04:03 -0400

    Add some documentation

--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,30 @@
 import express from 'express'
 import mongoose from 'mongoose'
 
-const PORT = 8080
-const connectionString: string = process.env.CONNECTIONSTRING || 'mongodb://localhost/taggerkeeper'
+// Parse environment variables into something useful
+const { PORT, CONNECTIONSTRING } = process.env
+const port: number = PORT ? parseInt(PORT) : 8080
+const connectionString: string = CONNECTIONSTRING || 'mongodb://localhost/taggerkeeper'
 
+// Connect to MongoDB
 await mongoose.connect(connectionString)
 
 const api = express()
 
+// GET /
 api.get('/', (req, res) => {
   res.send({ message: 'Hello, world! '})
 })
 
-const server = api.listen(PORT, () => {
-  console.log(`Tagger Keeper API is now running at port ${PORT}`)
+// Start server
+const server = api.listen(port, () => {
+  console.log(`Tagger Keeper API is now running at port ${port}`)
 })
+
+/**
+ * Close the server gracefully.
+ * @param {*} signal - The signal passed from the process.
+ */
 
 const closeGracefully = (signal: any) => {
   console.log(`Recieved signal to terminate: ${signal}`)
