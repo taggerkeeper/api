fb593636b2f2b6fc7f72357cc3fc4b89ba299165
Jason Godesky <jefgodesky@gmail.com>
2022-07-17 12:42:19 -0400

    Fixing test for follow-up test
    Pushing with a failing test caused the CD to not build a new version, just like we wanted it to! Now I'm fixing the test, so I'm expecting it to build a new version when I push it this time.

--- src/utils/read-file.spec.ts
+++ src/utils/read-file.spec.ts
@@ -4,6 +4,6 @@ import readFile from './read-file.js'
 describe('readFile', () => {
   it('reads a file into a string', async () => {
     const actual = await readFile('../../LICENSE')
-    expect(typeof actual).not.to.equal('string')
+    expect(typeof actual).to.equal('string')
   })
 })
