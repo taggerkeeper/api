39dd89eae03b335b905e88034f5ba3e06903ec4e
Jason Godesky <jefgodesky@gmail.com>
2022-07-19 20:36:06 -0400

    OTP.enable

--- src/models/otp/otp.spec.ts
+++ src/models/otp/otp.spec.ts
@@ -2,6 +2,8 @@ import { expect } from 'chai'
 import OTP from './otp.js'
 
 describe('OTP', () => {
+  const secret = 'shhhhh'
+
   describe('constructor', () => {
     it('is not enabled by default', () => {
       const otp = new OTP()
@@ -11,6 +13,26 @@ describe('OTP', () => {
     it('has no secret by default', () => {
       const otp = new OTP()
       expect(otp.secret).to.equal(undefined)
+    })
+  })
+
+  describe('enable', () => {
+    it('sets the secret', () => {
+      const otp = new OTP()
+      otp.enable(secret)
+      expect(otp.secret).to.equal(secret)
+    })
+
+    it('sets that the OTP is enabled', () => {
+      const otp = new OTP()
+      otp.enable('shhhhh')
+      expect(otp.enabled).to.equal(true)
+    })
+
+    it('returns true', () => {
+      const otp = new OTP()
+      const val = otp.enable(secret)
+      expect(val).to.equal(true)
     })
   })
 })
--- src/models/otp/otp.ts
+++ src/models/otp/otp.ts
@@ -1,6 +1,12 @@
 class OTP {
   enabled: boolean = false
   secret?: string = undefined
+
+  enable (secret: string): boolean {
+    this.secret = secret
+    this.enabled = true
+    return this.enabled
+  }
 }
 
 export default OTP
