1fe385390bb8de47e9bb266557746b3c5283ea57
Jason Godesky <jefgodesky@gmail.com>
2022-07-19 21:04:01 -0400

    Password.encrypt

--- src/models/password/password.spec.ts
+++ src/models/password/password.spec.ts
@@ -9,4 +9,21 @@ describe('Password', () => {
       expect(pwd.hash).to.equal(plaintext)
     })
   })
+
+  describe('Static methods', () => {
+    describe('encrypt', () => {
+      it('hashes the password', () => {
+        const plaintext = 'password'
+        const hash = Password.encrypt(plaintext)
+        expect(hash).not.to.equal(plaintext)
+      })
+
+      it('long passwords work', () => {
+        const preface = 'bcrypt will only hash the first 72 bytes of a string. This preface is 79 bytes.'
+        const h1 = Password.encrypt(`${preface} This will be our first test password.`)
+        const h2 = Password.encrypt(`${preface} This will be the second password that we test.`)
+        expect(h1).not.to.equal(h2)
+      })
+    })
+  })
 })
--- src/models/password/password.ts
+++ src/models/password/password.ts
@@ -1,8 +1,15 @@
+import bcrypt from 'bcryptjs'
+const { genSaltSync, hashSync } = bcrypt
+
 class Password {
   hash: string = ''
 
   constructor (plaintext?: string) {
     this.hash = plaintext === undefined ? '' : plaintext
+  }
+
+  static encrypt (plaintext: string, saltRounds: number = 10): string {
+    return hashSync(plaintext, genSaltSync(saltRounds))
   }
 }
 
