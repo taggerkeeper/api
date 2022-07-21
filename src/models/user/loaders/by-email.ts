5ef1aa31cf5c790e839db23f8d435efb9c6fa45d
Jason Godesky <jefgodesky@gmail.com>
2022-07-20 20:54:17 -0400

    Add new User loader: loadUsersByEmail

---
+++ src/models/user/loaders/by-email.ts
@@ -0,0 +1,16 @@
+import User from '../user.js'
+import UserModel from '../model.js'
+import loadUserFromRecord from './from-record.js'
+
+const loadUsersByEmail = async (addr: string): Promise<User[]> => {
+  const records = await UserModel.find({ 'emails.addr': addr })
+  const users = []
+  for (const record of records) {
+    const verifiedEmails = record.emails.filter(email => email.addr === addr && email.verified)
+    const user = verifiedEmails.length > 0 ? loadUserFromRecord(record) : null
+    if (user !== null) users.push(user)
+  }
+  return users
+}
+
+export default loadUsersByEmail
