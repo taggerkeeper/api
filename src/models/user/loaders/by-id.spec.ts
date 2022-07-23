f6eaa77ea005e6dbac3043450eaa541b20c95ef8
Jason Godesky <jefgodesky@gmail.com>
2022-07-23 12:48:38 -0400

    User loader: loadUserById: Add unit tests

---
+++ src/models/user/loaders/by-id.spec.ts
@@ -0,0 +1,38 @@
+import { expect } from 'chai'
+import * as sinon from 'sinon'
+import User from '../user.js'
+import UserModel from '../model.js'
+import loadUserById from './by-id.js'
+
+describe('loadUserById', () => {
+  const id = '0123456789abcdef12345678'
+  const emails = [{ addr: 'test1@testing.com', verified: true }, { addr: 'test2@testing.com', verified: false }]
+  const otp = { enabled: true, secret: 'shhhh' }
+  const record = { _id: id, active: true, admin: false, password: 'hash', emails, otp }
+
+  afterEach(() => sinon.restore())
+
+  it('returns null if not given a valid ID', async () => {
+    sinon.stub(UserModel, 'findById').callsFake((): any => {
+      return new Promise(resolve => resolve(null))
+    })
+    const actual = await loadUserById(id)
+    expect(actual).to.equal(null)
+  })
+
+  it('returns null if given a valid ID that does not exist', async () => {
+    sinon.stub(UserModel, 'findById').callsFake((): any => {
+      return new Promise(resolve => resolve(null))
+    })
+    const actual = await loadUserById(id)
+    expect(actual).to.equal(null)
+  })
+
+  it('returns a user if given a valid, existing ID', async () => {
+    sinon.stub(UserModel, 'findById').callsFake((): any => {
+      return new Promise(resolve => resolve(record))
+    })
+    const actual = await loadUserById(id)
+    expect(actual).to.be.an.instanceOf(User)
+  })
+})
