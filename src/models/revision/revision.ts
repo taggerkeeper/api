262777e265cee8e11ab72abe64f873efb6288720
Jason Godesky <jefgodesky@gmail.com>
2022-08-01 16:39:48 -0400

    Revision: Make editor optional
    Anonymous revisions are possible

--- src/models/revision/revision.ts
+++ src/models/revision/revision.ts
@@ -4,23 +4,23 @@ import Permissions from '../permissions/permissions.js'
 
 interface IRevision {
   content: Content
-  editor: User
   permissions?: Permissions
+  editor?: User
   msg?: string
   timestamp?: Date
 }
 
 class Revision {
   content: Content
-  editor: User
   permissions: Permissions
+  editor?: User
   msg: string
   timestamp: Date
 
   constructor (options: IRevision) {
     this.content = options.content
-    this.editor = options.editor
     this.permissions = options.permissions ?? new Permissions()
+    this.editor = options.editor
     this.msg = options.msg ?? ''
     this.timestamp = options.timestamp ?? new Date()
   }
