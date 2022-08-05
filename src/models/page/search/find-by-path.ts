0958f25e129cc037c5fd073af5695f699bf2c4e5
Jason Godesky <jefgodesky@gmail.com>
2022-08-05 16:52:22 -0400

    findByPath: Return Page

--- src/models/page/search/find-by-path.ts
+++ src/models/page/search/find-by-path.ts
@@ -1,11 +1,12 @@
 import PageModel from '../model.js'
-import PageData from '../data.js'
+import Page from '../page.js'
 import User from '../../user/user.js'
 import getSecuritySubquery from './get-security-subquery.js'
 
-const findByPath = async (path: string, searcher?: User): Promise<PageData | undefined> => {
+const findByPath = async (path: string, searcher?: User): Promise<Page | undefined> => {
   const query = Object.assign({}, getSecuritySubquery(searcher), { path })
-  return PageModel.findOne(query)
+  const record = await PageModel.findOne(query)
+  return record === null ? undefined : new Page(record)
 }
 
 export default findByPath
