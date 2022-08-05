f7bd75d2bbdb272a6c15abe76e063816de737cdc
Jason Godesky <jefgodesky@gmail.com>
2022-08-05 17:11:15 -0400

    renderMarkdown: Dealing with HTML

--- src/render/render-markdown.spec.ts
+++ src/render/render-markdown.spec.ts
@@ -6,4 +6,49 @@ describe('renderMarkdown', () => {
     const html = await renderMarkdown('*emphasis* and **strong**')
     expect(html).to.equal('<p><em>emphasis</em> and <strong>strong</strong></p>')
   })
+
+  it('allows basic HTML', async () => {
+    const html = await renderMarkdown('<span class="test">This is a test.</span>')
+    expect(html).to.equal('<p><span class="test">This is a test.</span></p>')
+  })
+
+  it('sanitizes dangerous HTML', async () => {
+    const html = await renderMarkdown('<script>This is a test.</script>')
+    expect(html).to.equal('')
+  })
+
+  it('allows divs', async () => {
+    const html = await renderMarkdown('<div class="test">This is a test.</div>')
+    expect(html).to.equal('<div class="test">This is a test.</div>')
+  })
+
+  it('allows blockquotes', async () => {
+    const html = await renderMarkdown('<blockquote>This is a test.</blockquote>')
+    expect(html).to.equal('<blockquote>This is a test.</blockquote>')
+  })
+
+  it('allows figures', async () => {
+    const html = await renderMarkdown('<figure><figcaption>This is a test.</figcaption></figure>')
+    expect(html).to.equal('<figure><figcaption>This is a test.</figcaption></figure>')
+  })
+
+  it('allows asides', async () => {
+    const html = await renderMarkdown('<aside>This is a test.</aside>')
+    expect(html).to.equal('<aside>This is a test.</aside>')
+  })
+
+  it('allows sections', async () => {
+    const html = await renderMarkdown('<section>This is a test.</section>')
+    expect(html).to.equal('<section>This is a test.</section>')
+  })
+
+  it('allows headers', async () => {
+    const html = await renderMarkdown('<header>This is a test.</header>')
+    expect(html).to.equal('<header>This is a test.</header>')
+  })
+
+  it('allows footers', async () => {
+    const html = await renderMarkdown('<footer>This is a test.</footer>')
+    expect(html).to.equal('<footer>This is a test.</footer>')
+  })
 })
--- src/render/render-markdown.ts
+++ src/render/render-markdown.ts
@@ -1,11 +1,21 @@
+import deepmerge from 'deepmerge'
 import { unified } from 'unified'
 import remarkParse from 'remark-parse'
 import remarkRehype from 'remark-rehype'
+import rehypeRaw from 'rehype-raw'
+import rehypeSanitize, { defaultSchema } from 'rehype-sanitize'
 import rehypeStringify from 'rehype-stringify'
+
+const schema = deepmerge(defaultSchema, {
+  tagNames: ['header', 'section', 'aside', 'footer'],
+  attributes: { '*': ['className'] }
+})
 
 const processor = unified()
   .use(remarkParse)
-  .use(remarkRehype)
+  .use(remarkRehype, { allowDangerousHtml: true })
+  .use(rehypeRaw)
+  .use(rehypeSanitize, schema)
   .use(rehypeStringify)
 
 const renderMarkdown = async (markdown: string): Promise<string> => {
