a5eff445aed26f900375b33fc229dd34b4b4d0d7
Jason Godesky <jefgodesky@gmail.com>
2022-08-04 20:42:04 -0400

    Add new utility method: getSecuritySubquery

---
+++ src/utils/get-security-subquery.spec.ts
@@ -0,0 +1,30 @@
+import { expect } from 'chai'
+import User from '../models/user/user.js'
+import getSecuritySubquery from './get-security-subquery.js'
+
+describe('getSecuritySubquery', () => {
+  const anyoneJson = '{"revisions[0].permissions.read":"anyone"}'
+
+  it('just checks for pages that can be anonymously read if not given a user', () => {
+    expect(JSON.stringify(getSecuritySubquery())).to.equal(anyoneJson)
+  })
+
+  it('just checks for pages that can be anonymously read if given undefined', () => {
+    expect(JSON.stringify(getSecuritySubquery(undefined))).to.equal(anyoneJson)
+  })
+
+  it('returns an empty object if the user is an admin', () => {
+    const admin = new User({ admin: true })
+    expect(JSON.stringify(getSecuritySubquery(admin))).to.equal('{}')
+  })
+
+  it('returns permissions logic for other users', () => {
+    const id = '0123456789abcdef12345678'
+    const searcher = new User({ id })
+    const authenticatedJson = '{"revisions[0].permissions.read":"authenticated"}'
+    const editorLevelJson = '{"revisions[0].permissions.read":"editor"}'
+    const isEditorJson = `{"revisions.editor":"${id}"}`
+    const json = `{"$or":[${anyoneJson},${authenticatedJson},{"$and":[${editorLevelJson},${isEditorJson}]}]}`
+    expect(JSON.stringify(getSecuritySubquery(searcher))).to.equal(json)
+  })
+})
---
+++ src/utils/get-security-subquery.ts
@@ -0,0 +1,19 @@
+import { PermissionLevel } from '../models/permissions/data.js'
+import User from '../models/user/user.js'
+import exists from './exists.js'
+
+const getSecuritySubquery = (searcher?: User): any => {
+  if (searcher?.admin === true) return {}
+
+  const levels = {
+    anyone: { 'revisions[0].permissions.read': PermissionLevel.anyone },
+    authenticated: { 'revisions[0].permissions.read': PermissionLevel.authenticated },
+    editor: { 'revisions[0].permissions.read': PermissionLevel.editor }
+  }
+  const isEditor = { 'revisions.editor': searcher?.id }
+
+  if (!exists(searcher)) return levels.anyone
+  return { $or: [levels.anyone, levels.authenticated, { $and: [levels.editor, isEditor] }] }
+}
+
+export default getSecuritySubquery
