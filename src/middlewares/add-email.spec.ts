31d1063d3b57232aacdc3d56d40023fb6d2fc79f
Jason Godesky <jefgodesky@gmail.com>
2022-08-07 13:45:41 -0400

    Add new middleware: addEmail

---
+++ src/middlewares/add-email.spec.ts
@@ -0,0 +1,38 @@
+import { expect } from 'chai'
+import { mockRequest, mockResponse } from 'mock-req-res'
+import User from '../models/user/user.js'
+import Email from '../models/email/email.js'
+import addEmail from './add-email.js'
+
+describe('addEmail', () => {
+  let mockReq = mockRequest()
+  let mockRes = mockResponse()
+  let mockNext = (): void => {}
+  const name = 'Tester'
+  const addr = 'tester@taggerkeeper.com'
+
+  beforeEach(() => {
+    mockReq = mockRequest()
+    mockRes = mockResponse()
+    mockNext = () => {}
+    mockReq.subject = new User({ name })
+    mockReq.body = { email: addr }
+    addEmail(mockReq, mockRes, mockNext)
+  })
+
+  it('adds to the request subject\'s array of emails', () => {
+    expect(mockReq.subject?.emails).to.have.lengthOf(1)
+  })
+
+  it('adds an Email instance to the request subject\'s array of emails', () => {
+    expect(mockReq.subject?.emails[0]).to.be.an.instanceOf(Email)
+  })
+
+  it('sets the address for the request subject\'s new email', () => {
+    expect(mockReq.subject?.emails[0].addr).to.equal(addr)
+  })
+
+  it('creates an unverified email', () => {
+    expect(mockReq.subject?.emails[0].verified).to.equal(false)
+  })
+})
---
+++ src/middlewares/add-email.ts
@@ -0,0 +1,11 @@
+import { Request, Response, NextFunction } from 'express'
+import Email from '../models/email/email.js'
+
+const addEmail = function (req: Request, res: Response, next: NextFunction): void {
+  if (req.subject !== undefined) {
+    const email = new Email({ addr: req.body.email })
+    req.subject.emails = [...req.subject.emails, email]
+  }
+}
+
+export default addEmail
