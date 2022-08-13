e1cdd0dd6f362c504b0f615c19734bdc8fd66cdd
Jason Godesky <jefgodesky@gmail.com>
2022-08-13 11:16:13 -0400

    Add new email method: getClient
    Returns a Mailgun.js client

---
+++ src/emails/get-client.ts
@@ -0,0 +1,16 @@
+import formData from 'form-data'
+import Mailgun from 'mailgun.js'
+import getEnvVar from '../utils/get-env-var.js'
+
+const getClient = () => {
+  const username = getEnvVar('MAILGUN_USERNAME') as string
+  const key = getEnvVar('MAILGUN_APIKEY') as string
+  const url = getEnvVar('MAILGUN_API') as string
+  const domain = getEnvVar('MAILGUN_DOMAIN') as string
+  const emailFrom = getEnvVar('EMAIL_FROM') as string
+
+  const mailgun = new Mailgun(formData)
+  return mailgun.client({ username, key, url })
+}
+
+export default getClient
