b747cbde1e14b351aad469418ba3722803c9cba9
Jason Godesky <jefgodesky@gmail.com>
2022-08-13 11:17:44 -0400

    sendMail: Use getClient

--- src/emails/send.ts
+++ src/emails/send.ts
@@ -1,18 +1,12 @@
-import formData from 'form-data'
-import Mailgun from 'mailgun.js'
 import getEnvVar from '../utils/get-env-var.js'
+import getClient from './get-client.js'
 
 const sendMail = async (emailTo: string, subject: string, text: string): Promise<boolean> => {
-  const username = getEnvVar('MAILGUN_USERNAME') as string
-  const key = getEnvVar('MAILGUN_APIKEY') as string
-  const url = getEnvVar('MAILGUN_API') as string
   const domain = getEnvVar('MAILGUN_DOMAIN') as string
   const emailFrom = getEnvVar('EMAIL_FROM') as string
 
-  const mailgun = new Mailgun(formData)
-  const client = mailgun.client({ username, key, url })
-
   try {
+    const client = getClient()
     await client.messages.create(domain, {
       from: emailFrom,
       to: emailTo,
