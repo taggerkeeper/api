2a109b4b46d175405872227dc1532a97ff706444
Jason Godesky <jefgodesky@gmail.com>
2022-08-15 20:35:34 -0400

    Express Express request to include req.email

--- src/types/express/index.d.ts
+++ src/types/express/index.d.ts
@@ -1,9 +1,11 @@
+import Email from '../../models/email/email.js'
 import User from '../../models/user/user.js'
 
 declare global {
   namespace Express {
     interface Request {
       subject?: User
+      email?: Email
     }
   }
 }
