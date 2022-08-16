77025d3728f1c1ad3acf57e14891dc16a8104ac1
Jason Godesky <jefgodesky@gmail.com>
2022-08-15 20:27:08 -0400

    Utility method: readFile: It's synchronous

--- src/utils/read-file.spec.ts
+++ src/utils/read-file.spec.ts
@@ -2,8 +2,8 @@ import { expect } from 'chai'
 import readFile from './read-file.js'
 
 describe('readFile', () => {
-  it('reads a file into a string', async () => {
-    const actual = await readFile('../../LICENSE')
+  it('reads a file into a string', () => {
+    const actual = readFile('../../LICENSE')
     expect(typeof actual).to.equal('string')
   })
 })
--- src/utils/read-file.ts
+++ src/utils/read-file.ts
@@ -2,12 +2,11 @@ import { readFileSync } from 'fs'
 import { dirname, resolve } from 'path'
 import { fileURLToPath } from 'url'
 
-const readFile = async (relativePath: string, encoding: BufferEncoding = 'utf8'): Promise<string> => {
+const readFile = (relativePath: string, encoding: BufferEncoding = 'utf8'): string => {
   try {
     const dir = dirname(fileURLToPath(import.meta.url))
     const path = resolve(dir, relativePath)
-    const content = await readFileSync(path, { encoding })
-    return content
+    return readFileSync(path, { encoding })
   } catch (err) {
     console.error(err)
     return '#ERROR'
