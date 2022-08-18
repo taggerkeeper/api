7f3d17a5bd0c2d939e49b9ce2b543f77e60790f6
Jason Godesky <jefgodesky@gmail.com>
2022-08-18 19:57:02 -0400

    Email method: sendReset: Add unit test (and dependency injection)

---
+++ src/emails/reset.spec.ts
@@ -0,0 +1,23 @@
+import chai, { expect } from 'chai'
+import * as sinon from 'sinon'
+import sinonChai from 'sinon-chai'
+import Email from '../models/email/email.js'
+import User from '../models/user/user.js'
+import PasswordReset from '../models/password-reset/password-reset.js'
+import sendReset from './reset.js'
+
+chai.use(sinonChai)
+
+describe('sendReset', () => {
+  let sender = sinon.stub().resolves(true)
+  beforeEach(() => { sender = sinon.stub().resolves(true) })
+  afterEach(() => sinon.restore())
+
+  it('sends a reset email', async () => {
+    const user = new User()
+    const email = new Email({ addr: 'test@testing.com', verified: true })
+    const reset = new PasswordReset({ user: user.getObj(), email: email.getObj() })
+    await sendReset(reset, '127.0.0.1', sender)
+    expect(sender.firstCall.args[1]).to.equal('Reset your password')
+  })
+})
--- src/emails/reset.ts
+++ src/emails/reset.ts
@@ -4,12 +4,13 @@ import renderStrVars from '../render/render-str-vars.js'
 import getClient from './get-client.js'
 import sendMail from './send.js'
 
-const sendReset = async (reset: PasswordReset, ipaddr: string): Promise<boolean> => {
+const sendReset = async (reset: PasswordReset, ipaddr: string, sender?: Function): Promise<boolean> => {
+  const sendMailFn = sender ?? sendMail
   const client = getClient()
   const { addr, verified } = reset.email
   if (!verified || addr === undefined) return false
   const text = renderStrVars(readFile('../../emails/reset.txt'), { resetcode: reset.code, ipaddr })
-  return await sendMail(addr, 'Reset your password', text)
+  return await sendMailFn(addr, 'Reset your password', text)
 }
 
 export default sendReset
