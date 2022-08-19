4a94f6e838bf33be0c4f68baaa69da05f9e0213e
Jason Godesky <jefgodesky@gmail.com>
2022-08-18 20:04:34 -0400

    Email method: sendVerification: Add unit test (and dependency injection)

---
+++ src/emails/verify.spec.ts
@@ -0,0 +1,19 @@
+import chai, { expect } from 'chai'
+import * as sinon from 'sinon'
+import sinonChai from 'sinon-chai'
+import Email from '../models/email/email.js'
+import sendVerification from './verify.js'
+
+chai.use(sinonChai)
+
+describe('sendVerification', () => {
+  let sender = sinon.stub().resolves(true)
+  beforeEach(() => { sender = sinon.stub().resolves(true) })
+  afterEach(() => sinon.restore())
+
+  it('sends a verification email', async () => {
+    const email = new Email({ addr: 'test@testing.com' })
+    await sendVerification(email, '127.0.0.1', sender)
+    expect(sender.firstCall.args[1]).to.equal('Can you verify this email address?')
+  })
+})
