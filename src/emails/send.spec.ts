3c4c7f9a11676b3e119258ac863e2c7f4a036ed4
Jason Godesky <jefgodesky@gmail.com>
2022-08-20 11:30:44 -0400

    Email method: sendMail: Drop validation
    Validation is only available to paid accounts, and I'm not going to pay extra to provide a safeguard to avoid stressing their API when this is all part of the real validation of just sending an email and seeing if it goes through.

--- src/emails/send.spec.ts
+++ src/emails/send.spec.ts
@@ -1,9 +1,6 @@
-import chai, { expect } from 'chai'
+import { expect } from 'chai'
 import * as sinon from 'sinon'
-import sinonChai from 'sinon-chai'
 import sendMail from './send.js'
-
-chai.use(sinonChai)
 
 describe('sendMail', () => {
   const emailTo = 'test@testing.com'
@@ -11,21 +8,14 @@ describe('sendMail', () => {
   const text = 'This is only a test.'
 
   const client = {
-    messages: { create: sinon.stub() },
-    validate: { get: sinon.stub().resolves({ is_valid: true }) }
+    messages: { create: sinon.stub() }
   }
 
   beforeEach(() => {
     client.messages.create = sinon.stub()
-    client.validate.get = sinon.stub().resolves({ is_valid: true })
   })
 
   afterEach(() => sinon.restore())
-
-  it('validates the email address first', async () => {
-    await sendMail(emailTo, subject, text, client)
-    expect(client.validate.get).to.have.been.calledBefore(client.messages.create)
-  })
 
   it('sends the email', async () => {
     await sendMail(emailTo, subject, text, client)
--- src/emails/send.ts
+++ src/emails/send.ts
@@ -1,24 +1,19 @@
 import getEnvVar from '../utils/get-env-var.js'
 import getClient from './get-client.js'
 
-const sendMail = async (emailTo: string, subject: string, text: string, client?: { messages: { create: Function }, validate: { get: Function } }): Promise<boolean> => {
+const sendMail = async (emailTo: string, subject: string, text: string, client?: { messages: { create: Function } }): Promise<boolean> => {
   const domain = getEnvVar('MAILGUN_DOMAIN') as string
   const emailFrom = getEnvVar('EMAIL_FROM') as string
 
   try {
     const mailgun = client ?? getClient()
-    const validation = await mailgun.validate.get(emailTo)
-    if (validation.is_valid === true) {
-      await mailgun.messages.create(domain, {
-        from: emailFrom,
-        to: emailTo,
-        subject,
-        text
-      })
-      return true
-    } else {
-      return false
-    }
+    await mailgun.messages.create(domain, {
+      from: emailFrom,
+      to: emailTo,
+      subject,
+      text
+    })
+    return true
   } catch (err) {
     console.error(err)
     return false
