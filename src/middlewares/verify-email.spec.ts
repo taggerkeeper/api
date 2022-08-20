32471d0cfbb488aee64b0300cd3382fac51079a2
Jason Godesky <jefgodesky@gmail.com>
2022-08-20 13:47:30 -0400

    Middleware: verifyEmail: Redo to expect selectEmail to be used before it
    It's not really refactoring, because we're changing how it works.

--- src/middlewares/verify-email.spec.ts
+++ src/middlewares/verify-email.spec.ts
@@ -1,7 +1,6 @@
 import { expect } from 'chai'
 import { mockRequest, mockResponse } from 'mock-req-res'
 import User from '../models/user/user.js'
-import Email from '../models/email/email.js'
 import verifyEmail from './verify-email.js'
 
 describe('addEmail', () => {
@@ -16,29 +15,24 @@ describe('addEmail', () => {
     mockRes = mockResponse()
     mockNext = () => {}
     mockReq.subject = new User({ name, emails: [{ addr }] })
-    mockReq.subject.emails[0].generateVerificationCode()
+    mockReq.email = mockReq.subject.emails[0]
+    mockReq.email.generateVerificationCode()
   })
 
-  it('doesn\'t add an email to the request if the code doesn\'t match anything', () => {
-    mockReq.params = { code: mockReq.subject?.emails[0].code === 'abc123' ? 'xyz098' : 'abc123' }
+  it('doesn\'t verify the email if the code is incorrect', () => {
+    mockReq.body = { code: mockReq.subject?.emails[0].code === 'abc123' ? 'xyz098' : 'abc123' }
     verifyEmail(mockReq, mockRes, mockNext)
-    expect(mockReq.email).to.equal(undefined)
-  })
-
-  it('adds an email to the request if the code is correct', () => {
-    mockReq.params = { code: mockReq.subject?.emails[0].code as string }
-    verifyEmail(mockReq, mockRes, mockNext)
-    expect(mockReq.email).to.be.an.instanceOf(Email)
+    expect(mockReq.email?.verified).to.equal(false)
   })
 
   it('verifies the email if the code is correct', () => {
-    mockReq.params = { code: mockReq.subject?.emails[0].code as string }
+    mockReq.body = { code: mockReq.subject?.emails[0].code as string }
     verifyEmail(mockReq, mockRes, mockNext)
     expect(mockReq.email?.verified).to.equal(true)
   })
 
   it('verifies the email in the subject user record if the code is correct', () => {
-    mockReq.params = { code: mockReq.subject?.emails[0].code as string }
+    mockReq.body = { code: mockReq.subject?.emails[0].code as string }
     verifyEmail(mockReq, mockRes, mockNext)
     expect(mockReq.subject?.emails[0].verified).to.equal(true)
   })
--- src/middlewares/verify-email.ts
+++ src/middlewares/verify-email.ts
@@ -1,9 +1,8 @@
 import { Request, Response, NextFunction } from 'express'
 
 const verifyEmail = function (req: Request, res: Response, next: NextFunction): void {
-  if (req.subject !== undefined && req.params.code !== undefined) {
-    const emails = req.subject.emails.filter(email => email.verify(req.params.code))
-    if (emails.length > 0) req.email = emails[0]
+  if (req.email !== undefined && req.body.code !== undefined) {
+    req.email.verify(req.body.code)
   }
   next()
 }
