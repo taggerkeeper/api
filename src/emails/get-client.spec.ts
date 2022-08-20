fceedfc3712387e06cc133c0f0d15763c4206363
Jason Godesky <jefgodesky@gmail.com>
2022-08-20 10:50:44 -0400

    Email method: getClient: Unit test checks for client.validate.get

--- src/emails/get-client.spec.ts
+++ src/emails/get-client.spec.ts
@@ -2,8 +2,13 @@ import { expect } from 'chai'
 import getClient from './get-client.js'
 
 describe('getClient', () => {
-  it('returns a Mailgun client', () => {
+  it('returns a Mailgun client that can send email', () => {
     const client = getClient()
     expect(client.messages.create).to.be.a('function')
+  })
+
+  it('returns a Mailgun client that can validate an email address', () => {
+    const client = getClient()
+    expect(client.validate.get).to.be.a('function')
   })
 })
