5eafcdba13558584096762a7e6b90323780cae4c
Jason Godesky <jefgodesky@gmail.com>
2022-08-20 11:16:54 -0400

    Email method: sendMail: More direct dependency injection

--- src/emails/send.spec.ts
+++ src/emails/send.spec.ts
@@ -10,26 +10,26 @@ describe('sendMail', () => {
   const subject = 'Test'
   const text = 'This is only a test.'
 
-  const fns = {
-    emailer: sinon.stub(),
-    validator: sinon.stub().resolves({ is_valid: true })
+  const client = {
+    messages: { create: sinon.stub() },
+    validate: { get: sinon.stub().resolves({ is_valid: true }) }
   }
 
   beforeEach(() => {
-    fns.emailer = sinon.stub()
-    fns.validator = sinon.stub().resolves({ is_valid: true })
+    client.messages.create = sinon.stub()
+    client.validate.get = sinon.stub().resolves({ is_valid: true })
   })
 
   afterEach(() => sinon.restore())
 
   it('validates the email address first', async () => {
-    await sendMail(emailTo, subject, text, fns)
-    expect(fns.validator).to.have.been.calledBefore(fns.emailer)
+    await sendMail(emailTo, subject, text, client)
+    expect(client.validate.get).to.have.been.calledBefore(client.messages.create)
   })
 
   it('sends the email', async () => {
-    await sendMail(emailTo, subject, text, fns)
-    const obj = fns.emailer.firstCall.args[1]
+    await sendMail(emailTo, subject, text, client)
+    const obj = client.messages.create.firstCall.args[1]
     expect([emailTo, subject, text].join(' ')).to.equal([obj.to, obj.subject, obj.text].join(' '))
   })
 })
--- src/emails/send.ts
+++ src/emails/send.ts
@@ -1,18 +1,15 @@
 import getEnvVar from '../utils/get-env-var.js'
 import getClient from './get-client.js'
 
-const sendMail = async (emailTo: string, subject: string, text: string, fns?: { emailer: Function, validator: Function }): Promise<boolean> => {
+const sendMail = async (emailTo: string, subject: string, text: string, client?: { messages: { create: Function }, validate: { get: Function } }): Promise<boolean> => {
   const domain = getEnvVar('MAILGUN_DOMAIN') as string
   const emailFrom = getEnvVar('EMAIL_FROM') as string
 
   try {
-    const client = fns === undefined ? getClient() : null
-    const emailer = fns?.emailer ?? client.messages.create
-    const validator = fns?.validator ?? client.validate.get
-
-    const validation = await validator(emailTo)
+    const mailgun = client ?? getClient()
+    const validation = await mailgun.validate.get(emailTo)
     if (validation.is_valid === true) {
-      await emailer(domain, {
+      await mailgun.messages.create(domain, {
         from: emailFrom,
         to: emailTo,
         subject,
