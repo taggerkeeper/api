8e505b2d6ffcf180c79f0d65f5f8655cdcdb7c8c
Jason Godesky <jefgodesky@gmail.com>
2022-08-21 15:48:14 -0400

    Add User to Express Request
    This is the user who's logged in, who won't always be the subject (the user that we're fetching something about).

--- src/types/express/index.d.ts
+++ src/types/express/index.d.ts
@@ -5,6 +5,7 @@ import User from '../../models/user/user.js'
 declare global {
   namespace Express {
     interface Request {
+      user?: User
       subject?: User
       email?: Email
       emailer?: Function | sinon.SinonStub
