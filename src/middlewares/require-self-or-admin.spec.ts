c82e295f46565da612a9fa223a82be8eff2a87a0
Jason Godesky <jefgodesky@gmail.com>
2022-08-27 17:15:40 -0400

    Add new middleware: requireSelfOrAdmin

---
+++ src/middlewares/require-self-or-admin.spec.ts
@@ -0,0 +1,68 @@
+import chai, { expect } from 'chai'
+import { mockRequest, mockResponse } from 'mock-req-res'
+import * as sinon from 'sinon'
+import sinonChai from 'sinon-chai'
+import User from '../models/user/user.js'
+import requireSelfOrAdmin from './require-self-or-admin.js'
+
+chai.use(sinonChai)
+
+describe('requireSelfOrAdmin', () => {
+  let mockReq = mockRequest()
+  let mockRes = mockResponse()
+  let mockNext = sinon.spy()
+
+  const data = { id: '0123456789abcdef01234567', name: 'Tester' }
+  const user = new User(data)
+  const subject = new User(data)
+  const other = new User({ id: '9876543210fedcba98765432', name: 'Other' })
+  const admin = new User({ id: 'abcdef012345678987654321', name: 'Admin', admin: true })
+
+  beforeEach(() => {
+    mockReq = mockRequest()
+    mockRes = mockResponse()
+    mockNext = sinon.spy()
+  })
+
+  it('returns an error if there\'s no user or subject', () => {
+    requireSelfOrAdmin(mockReq, mockRes, mockNext)
+    expect(mockRes.status.firstCall.args[0]).to.equal(401)
+    expect(mockRes.send.firstCall.args[0].message).to.equal('This method requires authentication by the subject or an administrator.')
+  })
+
+  it('returns an error if there\'s no user', () => {
+    mockReq.subject = subject
+    requireSelfOrAdmin(mockReq, mockRes, mockNext)
+    expect(mockRes.status.firstCall.args[0]).to.equal(401)
+    expect(mockRes.send.firstCall.args[0].message).to.equal('This method requires authentication by the subject or an administrator.')
+  })
+
+  it('returns an error if there\'s no subject', () => {
+    mockReq.user = user
+    requireSelfOrAdmin(mockReq, mockRes, mockNext)
+    expect(mockRes.status.firstCall.args[0]).to.equal(401)
+    expect(mockRes.send.firstCall.args[0].message).to.equal('This method requires authentication by the subject or an administrator.')
+  })
+
+  it('returns an error if the subject is different from the user, but the user isn\'t an admin', () => {
+    mockReq.user = other
+    mockReq.subject = subject
+    requireSelfOrAdmin(mockReq, mockRes, mockNext)
+    expect(mockRes.status.firstCall.args[0]).to.equal(401)
+    expect(mockRes.send.firstCall.args[0].message).to.equal('This method requires authentication by the subject or an administrator.')
+  })
+
+  it('calls the next middleware if the subject is the user', () => {
+    mockReq.user = user
+    mockReq.subject = subject
+    requireSelfOrAdmin(mockReq, mockRes, mockNext)
+    expect(mockNext).to.have.callCount(1)
+  })
+
+  it('calls the next middleware if the user is an admin', () => {
+    mockReq.user = admin
+    mockReq.subject = subject
+    requireSelfOrAdmin(mockReq, mockRes, mockNext)
+    expect(mockNext).to.have.callCount(1)
+  })
+})
---
+++ src/middlewares/require-self-or-admin.ts
@@ -0,0 +1,13 @@
+import { Request, Response, NextFunction } from 'express'
+
+const requireSelfOrAdmin = function (req: Request, res: Response, next: NextFunction): void {
+  const isAdmin = req.user?.admin === true
+  const isSubject = req.subject !== undefined && req.user !== undefined && req.subject?.id === req.user?.id
+  if (!isAdmin && !isSubject) {
+    res.status(401).send({ message: 'This method requires authentication by the subject or an administrator.' })
+  } else {
+    next()
+  }
+}
+
+export default requireSelfOrAdmin
