cddd49f73a3e8f502a412599827603f54caa139a
Jason Godesky <jefgodesky@gmail.com>
2022-08-27 16:59:06 -0400

    User loaders: Drop loadUserFromRecord
    We can just use the constructor on the User class, which seems to work better anyway.

--- src/models/user/loaders/by-email.ts
+++ src/models/user/loaders/by-email.ts
@@ -1,6 +1,5 @@
 import User from '../user.js'
 import UserModel from '../model.js'
-import loadUserFromRecord from './from-record.js'
 
 const loadUsersByEmail = async (addr: string): Promise<User[]> => {
   const records = await UserModel.find({ 'emails.addr': addr })
@@ -8,7 +7,7 @@ const loadUsersByEmail = async (addr: string): Promise<User[]> => {
   for (const record of records) {
     const allEmails = record.emails === undefined ? [] : record.emails
     const verifiedEmails = allEmails.filter(email => email.addr === addr && email.verified)
-    const user = verifiedEmails.length > 0 ? loadUserFromRecord(record) : null
+    const user = verifiedEmails.length > 0 ? new User(record) : null
     if (user !== null) users.push(user)
   }
   return users
--- src/models/user/loaders/by-id-and-refresh.ts
+++ src/models/user/loaders/by-id-and-refresh.ts
@@ -1,14 +1,13 @@
 import mongoose from 'mongoose'
 import User from '../user.js'
 import UserModel from '../model.js'
-import loadUserFromRecord from './from-record.js'
 const { isValid } = mongoose.Types.ObjectId
 
 const loadUserByIdAndRefresh = async (id: string, refresh: string): Promise<User | null> => {
   try {
     if (!isValid(id)) return null
     const record = await UserModel.findOne({ id, refresh })
-    return loadUserFromRecord(record)
+    return record === null ? null : new User(record)
   } catch (err) {
     console.error(err)
     return null
