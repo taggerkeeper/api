f5f26f8a07e860bb6a97a38ee5fffe38a6e99cd6
Jason Godesky <jefgodesky@gmail.com>
2022-08-31 18:21:22 -0400

    Add new utility method: getConnStr

---
+++ src/utils/get-conn-str.ts
@@ -0,0 +1,11 @@
+import getEnvVar from './get-env-var.js'
+
+const getConnStr = (): string => {
+  const server = getEnvVar('MONGO_SERVER') as string
+  const username = getEnvVar('MONGO_USERNAME') as string
+  const password = getEnvVar('MONGO_PASSWORD') as string
+  const collection = getEnvVar('MONGO_COLLECTION') as string
+  return `mongodb://${username}:${password}@${server}/${collection}`
+}
+
+export default getConnStr
