e8eeaf6dcc5931ac9d216f51c949d9c66d8fb23c
Jason Godesky <jefgodesky@gmail.com>
2022-09-08 20:39:27 -0400

    Utils: getConnStr: Can just pass through MONGO_CONN_STR if needed

--- src/utils/get-conn-str.ts
+++ src/utils/get-conn-str.ts
@@ -1,11 +1,12 @@
 import getEnvVar from './get-env-var.js'
 
 const getConnStr = (): string => {
+  const connstr = getEnvVar('MONGO_CONN_STR')
   const server = getEnvVar('MONGO_SERVER') as string
   const username = getEnvVar('MONGO_USERNAME') as string
   const password = getEnvVar('MONGO_PASSWORD') as string
   const collection = getEnvVar('MONGO_COLLECTION') as string
-  return `mongodb://${username}:${password}@${server}/${collection}`
+  return getEnvVar('MONGO_CONN_STR') as string ?? `mongodb://${username}:${password}@${server}/${collection}`
 }
 
 export default getConnStr
