bb4cbf68bcfc23648b7e0c46275898b3d0dbcba1
Jason Godesky <jefgodesky@gmail.com>
2022-09-08 20:44:16 -0400

    Utils: getConnStr: Clean up unused variable

--- src/utils/get-conn-str.ts
+++ src/utils/get-conn-str.ts
@@ -1,7 +1,6 @@
 import getEnvVar from './get-env-var.js'
 
 const getConnStr = (): string => {
-  const connstr = getEnvVar('MONGO_CONN_STR')
   const server = getEnvVar('MONGO_SERVER') as string
   const username = getEnvVar('MONGO_USERNAME') as string
   const password = getEnvVar('MONGO_PASSWORD') as string
