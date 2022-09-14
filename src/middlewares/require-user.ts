efbc8779228bc6f1c5425a829ec19a16d5f60ac0
Jason Godesky <jefgodesky@gmail.com>
2022-09-14 19:22:17 -0400

    Correct misspelling

--- src/middlewares/require-user.ts
+++ src/middlewares/require-user.ts
@@ -2,8 +2,8 @@ import { Request, Response, NextFunction } from 'express'
 
 const requireUser = function (req: Request, res: Response, next: NextFunction): void {
   const token = req.headers.authorization?.substring(7)
-  const tokenRecevied = token !== undefined && token.length > 0
-  if (req.user === undefined && !tokenRecevied) {
+  const tokenReceived = token !== undefined && token.length > 0
+  if (req.user === undefined && !tokenReceived) {
     res.status(400).send({ message: 'This method requires authentication.' })
   } else if (req.user === undefined) {
     res.set('WWW-Authenticate', 'Bearer error="invalid_token" error_description="The access token could not be verified."')
