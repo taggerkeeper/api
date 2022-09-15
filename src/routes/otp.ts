cec38cbf42ab9716da30bd685d80ceabdfa30c25
Jason Godesky <jefgodesky@gmail.com>
2022-09-15 18:48:32 -0400

    GET /otp
    Also HEAD

--- src/routes/otp.ts
+++ src/routes/otp.ts
@@ -1,4 +1,6 @@
 import { Request, Response, Router } from 'express'
+import expressAsyncHandler from 'express-async-handler'
+import OTP from '../models/otp/otp.js'
 
 import allow from '../middlewares/allow.js'
 import loadUserFromAccessToken from '../middlewares/load-user-from-access-token.js'
@@ -9,7 +11,21 @@ const router = Router()
 const otp = {
   options: (req: Request, res: Response) => {
     res.sendStatus(204)
-  }
+  },
+  head: (req: Request, res: Response) => {
+    const status = req.user === undefined ? 500 : 204
+    res.sendStatus(status)
+  },
+  get: expressAsyncHandler(async (req: Request, res: Response) => {
+    const secret = await OTP.generate()
+    if (req.user !== undefined) {
+      req.user.otp.secret = secret.base32
+      await req.user.save()
+      res.status(200).send(secret.qr)
+    } else {
+      res.status(500).json({ message: 'No user found.' })
+    }
+  })
 }
 
 router.all('/', allow(otp))
@@ -18,7 +34,7 @@ router.all('/', allow(otp))
  * @openapi
  * /otp:
  *   options:
- *     summary: "Methods for the OTP collection."
+ *     summary: "Methods for the OTP endpoint."
  *     description: "This method returns an Allow header which lists the methods that this endpoint allows."
  *     tags:
  *       - otp
@@ -77,5 +93,151 @@ router.all('/', allow(otp))
  */
 
 router.options('/', loadUserFromAccessToken, requireUser, otp.options)
+
+/**
+ * @openapi
+ * /otp:
+ *   head:
+ *     summary: "Returns the headers for the OTP endpoint."
+ *     description: "Returns the headers that a user would receive if requesting the OTP endpoint."
+ *     tags:
+ *       - otp
+ *     responses:
+ *       204:
+ *         headers:
+ *           'Allow':
+ *             schema:
+ *               type: string
+ *               example: "OPTIONS, HEAD"
+ *             description: "The methods that this endpoint allows."
+ *           'Access-Control-Allow-Methods':
+ *             schema:
+ *               type: string
+ *               example: "OPTIONS, HEAD"
+ *             description: "The methods that this endpoint allows."
+ *       400:
+ *         description: "This typically happens when you make a request without an authorization header."
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   description: "A description of the error that occurred."
+ *                   example "This method requires authentication."
+ *       401:
+ *         description: "This typically happens when you call a request with an authorization header that does not include a verifiable access token."
+ *         headers:
+ *           'WWW-Authenticate':
+ *             schema:
+ *               type: string
+ *               example: 'Bearer error="invalid_token" error_description="The access token could not be verified."'
+ *             description: "A description of what you need to authenticate. See `POST /tokens` for the method necessary to obtain an access token. This token should be passed to the method in a Bearer Authorization header."
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   description: "A description of the error that occurred."
+ *                   example "This method requires authentication."
+ *       403:
+ *         description: "This typically happen when you make a request with a deactivated user account."
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   description: "A description of the error that occurred."
+ *                   example: "Your account has been deactivated."
+ *       500:
+ *         description: "The server encountered an unexpected error."
+ */
+
+router.head('/', loadUserFromAccessToken, requireUser, otp.head)
+
+/**
+ * @openapi
+ * /otp:
+ *   get:
+ *     summary: "Get a one-time-password secret."
+ *     description: "Generates a new one-time-password secret and returns a QR code for the user to save and verify. The one-time-password will not be enabled on the account until the user has verified receipt of the secret using the `POST /otp` method."
+ *     tags:
+ *       - otp
+ *     responses:
+ *       200:
+ *         headers:
+ *           'Allow':
+ *             schema:
+ *               type: string
+ *               example: "OPTIONS, HEAD"
+ *             description: "The methods that this endpoint allows."
+ *           'Access-Control-Allow-Methods':
+ *             schema:
+ *               type: string
+ *               example: "OPTIONS, HEAD"
+ *             description: "The methods that this endpoint allows."
+ *         content:
+ *           text/plain:
+ *             schema:
+ *               type: string
+ *               description: "A QR code that the user can use to save the one-time-password secret."
+ *       400:
+ *         description: "This typically happens when you make a request without an authorization header."
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   description: "A description of the error that occurred."
+ *                   example "This method requires authentication."
+ *       401:
+ *         description: "This typically happens when you call a request with an authorization header that does not include a verifiable access token."
+ *         headers:
+ *           'WWW-Authenticate':
+ *             schema:
+ *               type: string
+ *               example: 'Bearer error="invalid_token" error_description="The access token could not be verified."'
+ *             description: "A description of what you need to authenticate. See `POST /tokens` for the method necessary to obtain an access token. This token should be passed to the method in a Bearer Authorization header."
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   description: "A description of the error that occurred."
+ *                   example "This method requires authentication."
+ *       403:
+ *         description: "This typically happen when you make a request with a deactivated user account."
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   description: "A description of the error that occurred."
+ *                   example: "Your account has been deactivated."
+ *       500:
+ *         description: "The server encountered an unexpected error."
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   description: "A description of the error that occurred."
+ *                   example: "No user found."
+ */
+
+router.get('/', loadUserFromAccessToken, requireUser, otp.get)
 
 export default router
--- src/routes/tests/otp.ts
+++ src/routes/tests/otp.ts
@@ -1,6 +1,8 @@
 import { expect } from 'chai'
 import request from 'supertest'
+import validDataUrl from 'valid-data-url'
 import User, { TokenSet } from '../../models/user/user.js'
+import loadUserById from '../../models/user/loaders/by-id.js'
 import loadPackage, { NPMPackage } from '../../utils/load-package.js'
 import getAPIInfo from '../../utils/get-api-info.js'
 import api from '../../server.js'
@@ -11,7 +13,7 @@ describe('OTP API', () => {
   let res: any
   const user = new User()
   let tokens: TokenSet
-  const allow = 'OPTIONS'
+  const allow = 'OPTIONS, HEAD, GET'
 
   beforeEach(async () => {
     await user.save()
@@ -87,6 +89,145 @@ describe('OTP API', () => {
 
         it('returns Access-Control-Allow-Methods header', () => {
           expect(res.headers['access-control-allow-methods']).to.equal(allow)
+        })
+      })
+    })
+
+    describe('HEAD /otp', () => {
+      describe('No authorization', () => {
+        beforeEach(async () => {
+          res = await request(api).head(`${base}/otp`)
+        })
+
+        it('returns 400', () => {
+          expect(res.status).to.equal(400)
+        })
+
+        it('returns Allow header', () => {
+          expect(res.headers.allow).to.equal(allow)
+        })
+
+        it('returns Access-Control-Allow-Methods header', () => {
+          expect(res.headers['access-control-allow-methods']).to.equal(allow)
+        })
+      })
+
+      describe('Deactivated user authorization', () => {
+        beforeEach(async () => {
+          const deactivated = new User({ name: 'Deactivated User', active: false })
+          await deactivated.save()
+          tokens = await deactivated.generateTokens()
+          await deactivated.save()
+          res = await request(api).head(`${base}/otp`).set({ Authorization: `Bearer ${tokens.access}` })
+        })
+
+        it('returns 403', () => {
+          expect(res.status).to.equal(403)
+        })
+
+        it('returns Allow header', () => {
+          expect(res.headers.allow).to.equal(allow)
+        })
+
+        it('returns Access-Control-Allow-Methods header', () => {
+          expect(res.headers['access-control-allow-methods']).to.equal(allow)
+        })
+      })
+
+      describe('Proper authorization', () => {
+        beforeEach(async () => {
+          res = await request(api).head(`${base}/otp`).set({ Authorization: `Bearer ${tokens.access}` })
+        })
+
+        it('returns 204', () => {
+          expect(res.status).to.equal(204)
+        })
+
+        it('returns Allow header', () => {
+          expect(res.headers.allow).to.equal(allow)
+        })
+
+        it('returns Access-Control-Allow-Methods header', () => {
+          expect(res.headers['access-control-allow-methods']).to.equal(allow)
+        })
+      })
+    })
+
+    describe('GET /otp', () => {
+      describe('No authorization', () => {
+        beforeEach(async () => {
+          res = await request(api).get(`${base}/otp`)
+        })
+
+        it('returns 400', () => {
+          expect(res.status).to.equal(400)
+        })
+
+        it('returns Allow header', () => {
+          expect(res.headers.allow).to.equal(allow)
+        })
+
+        it('returns Access-Control-Allow-Methods header', () => {
+          expect(res.headers['access-control-allow-methods']).to.equal(allow)
+        })
+
+        it('returns an error message', () => {
+          expect(res.body.message).to.equal('This method requires authentication.')
+        })
+      })
+
+      describe('Deactivated user authorization', () => {
+        beforeEach(async () => {
+          const deactivated = new User({ name: 'Deactivated User', active: false })
+          await deactivated.save()
+          tokens = await deactivated.generateTokens()
+          await deactivated.save()
+          res = await request(api).get(`${base}/otp`).set({ Authorization: `Bearer ${tokens.access}` })
+        })
+
+        it('returns 403', () => {
+          expect(res.status).to.equal(403)
+        })
+
+        it('returns Allow header', () => {
+          expect(res.headers.allow).to.equal(allow)
+        })
+
+        it('returns Access-Control-Allow-Methods header', () => {
+          expect(res.headers['access-control-allow-methods']).to.equal(allow)
+        })
+
+        it('returns an error message', () => {
+          expect(res.body.message).to.equal('Your account has been deactivated.')
+        })
+      })
+
+      describe('Proper authorization', () => {
+        beforeEach(async () => {
+          res = await request(api).get(`${base}/otp`).set({ Authorization: `Bearer ${tokens.access}` })
+        })
+
+        it('returns 200', () => {
+          expect(res.status).to.equal(200)
+        })
+
+        it('returns Allow header', () => {
+          expect(res.headers.allow).to.equal(allow)
+        })
+
+        it('returns Access-Control-Allow-Methods header', () => {
+          expect(res.headers['access-control-allow-methods']).to.equal(allow)
+        })
+
+        it('returns a QR code', () => {
+          expect(validDataUrl(res.text)).to.equal(true)
+          expect(1 + 1).to.equal(2)
+        })
+
+        it('stores the secret to the user account', async () => {
+          const after = await loadUserById(user.id ?? '')
+          expect(after?.otp.secret).not.to.equal(undefined)
+          expect(after?.otp.enabled).to.equal(false)
         })
       })
     })
