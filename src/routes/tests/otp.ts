f38dbe12f655a9fed6024f9087941841d8115742
Jason Godesky <jefgodesky@gmail.com>
2022-09-15 19:10:09 -0400

    POST /otp

--- src/routes/tests/otp.ts
+++ src/routes/tests/otp.ts
@@ -1,7 +1,9 @@
 import { expect } from 'chai'
 import request from 'supertest'
 import validDataUrl from 'valid-data-url'
+import speakeasy from 'speakeasy'
 import User, { TokenSet } from '../../models/user/user.js'
+import OTP from '../../models/otp/otp.js'
 import loadUserById from '../../models/user/loaders/by-id.js'
 import loadPackage, { NPMPackage } from '../../utils/load-package.js'
 import getAPIInfo from '../../utils/get-api-info.js'
@@ -13,7 +15,7 @@ describe('OTP API', () => {
   let res: any
   const user = new User()
   let tokens: TokenSet
-  const allow = 'OPTIONS, HEAD, GET'
+  const allow = 'OPTIONS, HEAD, GET, POST'
 
   beforeEach(async () => {
     await user.save()
@@ -221,13 +223,103 @@ describe('OTP API', () => {
 
         it('returns a QR code', () => {
           expect(validDataUrl(res.text)).to.equal(true)
-          expect(1 + 1).to.equal(2)
         })
 
         it('stores the secret to the user account', async () => {
           const after = await loadUserById(user.id ?? '')
           expect(after?.otp.secret).not.to.equal(undefined)
           expect(after?.otp.enabled).to.equal(false)
+        })
+      })
+    })
+
+    describe('POST /otp', () => {
+      let code: string
+
+      beforeEach(async () => {
+        const { base32 } = await OTP.generate()
+        user.otp.enable(base32)
+        await user.save()
+        code = speakeasy.totp({ secret: base32, encoding: 'base32' })
+      })
+
+      describe('No authorization', () => {
+        beforeEach(async () => {
+          res = await request(api).post(`${base}/otp`).send({ code })
+        })
+
+        it('returns 400', () => {
+          expect(res.status).to.equal(400)
+        })
+
+        it('returns Allow header', () => {
+          expect(res.headers.allow).to.equal(allow)
+        })
+
+        it('returns Access-Control-Allow-Methods header', () => {
+          expect(res.headers['access-control-allow-methods']).to.equal(allow)
+        })
+
+        it('returns an error message', () => {
+          expect(res.body.message).to.equal('This method requires authentication.')
+        })
+      })
+
+      describe('Deactivated user authorization', () => {
+        beforeEach(async () => {
+          const deactivated = new User({ name: 'Deactivated User', active: false })
+          await deactivated.save()
+          tokens = await deactivated.generateTokens()
+          await deactivated.save()
+          res = await request(api).post(`${base}/otp`).set({ Authorization: `Bearer ${tokens.access}` }).send({ code })
+        })
+
+        it('returns 403', () => {
+          expect(res.status).to.equal(403)
+        })
+
+        it('returns Allow header', () => {
+          expect(res.headers.allow).to.equal(allow)
+        })
+
+        it('returns Access-Control-Allow-Methods header', () => {
+          expect(res.headers['access-control-allow-methods']).to.equal(allow)
+        })
+
+        it('returns an error message', () => {
+          expect(res.body.message).to.equal('Your account has been deactivated.')
+        })
+      })
+
+      describe('Proper authorization', () => {
+        beforeEach(async () => {
+          res = await request(api).post(`${base}/otp`).set({ Authorization: `Bearer ${tokens.access}` }).send({ code })
+        })
+
+        it('returns 200', () => {
+          expect(res.status).to.equal(200)
+        })
+
+        it('returns Allow header', () => {
+          expect(res.headers.allow).to.equal(allow)
+        })
+
+        it('returns Access-Control-Allow-Methods header', () => {
+          expect(res.headers['access-control-allow-methods']).to.equal(allow)
+        })
+
+        it('returns a message', () => {
+          expect(res.body.message).to.equal('OTP verified and enabled.')
+        })
+
+        it('returns your user ID', () => {
+          expect(res.body.id).to.equal(user.id)
+        })
+
+        it('enables OTP on the user account', async () => {
+          const after = await loadUserById(user.id ?? '')
+          expect(after?.otp.secret).not.to.equal(undefined)
+          expect(after?.otp.enabled).to.equal(true)
         })
       })
     })
