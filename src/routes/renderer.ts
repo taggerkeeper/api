9574b128956b3b0ddd842faf84ec66f8f76c2689
Jason Godesky <jefgodesky@gmail.com>
2022-09-17 10:46:02 -0400

    GET /renderer (and HEAD)

--- src/routes/renderer.ts
+++ src/routes/renderer.ts
@@ -1,4 +1,6 @@
 import { Request, Response, Router } from 'express'
+import expressAsyncHandler from 'express-async-handler'
+import Page from '../models/page/page.js'
 
 import allow from '../middlewares/allow.js'
 import loadUserFromAccessToken from '../middlewares/load-user-from-access-token.js'
@@ -8,7 +10,14 @@ const router = Router()
 const renderer = {
   options: (req: Request, res: Response) => {
     res.sendStatus(204)
-  }
+  },
+  head: (req: Request, res: Response) => {
+    res.sendStatus(200)
+  },
+  get: expressAsyncHandler(async (req: Request, res: Response) => {
+    const render = await Page.render(req.query.text?.toString() ?? '')
+    res.status(200).send(render)
+  })
 }
 
 router.all('/', allow(renderer))
@@ -20,22 +29,92 @@ router.all('/', allow(renderer))
  *     summary: "Methods for the renderer endpoint."
  *     description: "This method returns an Allow header which lists the methods that this endpoint allows."
  *     tags:
- *       - otp
+ *       - renderer
  *     responses:
  *       204:
  *         headers:
  *           'Allow':
  *             schema:
  *               type: string
- *               example: "OPTIONS"
+ *               example: "OPTIONS, HEAD, GET"
  *             description: "The methods that this endpoint allows."
  *           'Access-Control-Allow-Methods':
  *             schema:
  *               type: string
- *               example: "OPTIONS"
+ *               example: "OPTIONS, HEAD, GET"
  *             description: "The methods that this endpoint allows."
  */
 
 router.options('/', loadUserFromAccessToken, renderer.options)
+
+/**
+ * @openapi
+ * /renderer:
+ *   head:
+ *     summary: "Return headers for rendering text."
+ *     description: "Returns the headers that a user would receive if rendering text."
+ *     tags:
+ *       - renderer
+ *     parameters:
+ *       - in: query
+ *         name: text
+ *         schema:
+ *           type: string
+ *         description: "The text to be rendered."
+ *         example: "Some text is **bolded**, and some is in _italics_."
+ *     responses:
+ *       200:
+ *         headers:
+ *           'Allow':
+ *             schema:
+ *               type: string
+ *               example: "OPTIONS, HEAD, GET"
+ *             description: "The methods that this endpoint allows."
+ *           'Access-Control-Allow-Methods':
+ *             schema:
+ *               type: string
+ *               example: "OPTIONS, HEAD, GET"
+ *             description: "The methods that this endpoint allows."
+ */
+
+router.head('/', loadUserFromAccessToken, renderer.head)
+
+/**
+ * @openapi
+ * /renderer:
+ *   get:
+ *     summary: "Render text."
+ *     description: "This method renders a string of text as it would be rendered as the content of a page."
+ *     tags:
+ *       - renderer
+ *     parameters:
+ *       - in: query
+ *         name: text
+ *         schema:
+ *           type: string
+ *         description: "The text to be rendered."
+ *         example: "Some text is **bolded**, and some is in _italics_."
+ *     responses:
+ *       200:
+ *         headers:
+ *           'Allow':
+ *             schema:
+ *               type: string
+ *               example: "OPTIONS, HEAD, GET"
+ *             description: "The methods that this endpoint allows."
+ *           'Access-Control-Allow-Methods':
+ *             schema:
+ *               type: string
+ *               example: "OPTIONS, HEAD, GET"
+ *             description: "The methods that this endpoint allows."
+ *           content:
+ *             plain/text:
+ *               schema:
+ *                 type: string
+ *                 example: "<p>Some text is <strong>bolded</strong>, and some is in <em>italics</em>.</p>"
+ *
+ */
+
+router.get('/', loadUserFromAccessToken, renderer.get)
 
 export default router
--- src/routes/tests/renderer.ts
+++ src/routes/tests/renderer.ts
@@ -8,7 +8,9 @@ describe('Renderer API', () => {
   let pkg: NPMPackage
   let base: string
   let res: any
-  const allow = 'OPTIONS'
+  const allow = 'OPTIONS, HEAD, GET'
+  const text = 'Some text is **bolded**, and some is in _italics_.'
+  const expected = '<p>Some text is <strong>bolded</strong>, and some is in <em>italics</em>.</p>'
 
   beforeEach(async () => {
     pkg = await loadPackage() as NPMPackage
@@ -19,7 +21,7 @@ describe('Renderer API', () => {
   describe('/renderer', () => {
     describe('OPTIONS /renderer', () => {
       beforeEach(async () => {
-        res = await request(api).options(`${base}/renderer`)
+        res = await request(api).options(`${base}/renderer?text=${text}`)
       })
 
       it('returns 204', () => {
@@ -32,6 +34,46 @@ describe('Renderer API', () => {
 
       it('returns Access-Control-Allow-Methods header', () => {
         expect(res.headers['access-control-allow-methods']).to.equal(allow)
+      })
+    })
+
+    describe('HEAD /renderer', () => {
+      beforeEach(async () => {
+        res = await request(api).head(`${base}/renderer?text=${text}`)
+      })
+
+      it('returns 200', () => {
+        expect(res.status).to.equal(200)
+      })
+
+      it('returns Allow header', () => {
+        expect(res.headers.allow).to.equal(allow)
+      })
+
+      it('returns Access-Control-Allow-Methods header', () => {
+        expect(res.headers['access-control-allow-methods']).to.equal(allow)
+      })
+    })
+
+    describe('GET /renderer', () => {
+      beforeEach(async () => {
+        res = await request(api).get(`${base}/renderer?text=${text}`)
+      })
+
+      it('returns 200', () => {
+        expect(res.status).to.equal(200)
+      })
+
+      it('returns Allow header', () => {
+        expect(res.headers.allow).to.equal(allow)
+      })
+
+      it('returns Access-Control-Allow-Methods header', () => {
+        expect(res.headers['access-control-allow-methods']).to.equal(allow)
+      })
+
+      it('returns the rendered text', () => {
+        expect(res.text).to.equal(expected)
       })
     })
   })
