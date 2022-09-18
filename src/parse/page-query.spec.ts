fbb06aafa6c3f4af731450fa3353caa3907e27ef
Jason Godesky <jefgodesky@gmail.com>
2022-09-18 11:36:56 -0400

    Add new parser: parsePageQuery
    Parses an Express Request with specific query parameters into a PageQuery.

---
+++ src/parse/page-query.spec.ts
@@ -0,0 +1,378 @@
+import { expect } from 'chai'
+import { mockRequest } from 'mock-req-res'
+import parsePageQuery from './page-query.js'
+
+describe('parsePageQuery', () => {
+  let mockReq = mockRequest()
+
+  beforeEach(() => {
+    mockReq = mockRequest()
+  })
+
+  it('parses created before', () => {
+    const createdBefore = 1659312000000
+    mockReq.query = { 'created-before': createdBefore.toString() }
+
+    const query = parsePageQuery(mockReq)
+    expect(query.created?.before?.getTime()).to.equal(createdBefore)
+    expect(query.created?.after).to.equal(undefined)
+    expect(query.updated).to.equal(undefined)
+    expect(query.revisions).to.equal(undefined)
+    expect(query.text).to.equal(undefined)
+    expect(query.limit).to.equal(undefined)
+    expect(query.offset).to.equal(undefined)
+    expect(query.sort).to.equal(undefined)
+  })
+
+  it('parses created after', () => {
+    const createdAfter = 1659398400000
+    mockReq.query = { 'created-after': createdAfter.toString() }
+
+    const query = parsePageQuery(mockReq)
+    expect(query.created?.before).to.equal(undefined)
+    expect(query.created?.after?.getTime()).to.equal(createdAfter)
+    expect(query.updated).to.equal(undefined)
+    expect(query.revisions).to.equal(undefined)
+    expect(query.text).to.equal(undefined)
+    expect(query.limit).to.equal(undefined)
+    expect(query.offset).to.equal(undefined)
+    expect(query.sort).to.equal(undefined)
+  })
+
+  it('parses both created dates', () => {
+    const createdBefore = 1659312000000
+    const createdAfter = 1659398400000
+
+    mockReq.query = {
+      'created-before': createdBefore.toString(),
+      'created-after': createdAfter.toString()
+    }
+
+    const query = parsePageQuery(mockReq)
+    expect(query.created?.before?.getTime()).to.equal(createdBefore)
+    expect(query.created?.after?.getTime()).to.equal(createdAfter)
+    expect(query.updated).to.equal(undefined)
+    expect(query.revisions).to.equal(undefined)
+    expect(query.text).to.equal(undefined)
+    expect(query.limit).to.equal(undefined)
+    expect(query.offset).to.equal(undefined)
+    expect(query.sort).to.equal(undefined)
+  })
+
+  it('parses updated before', () => {
+    const updatedBefore = 1659484800000
+    mockReq.query = { 'updated-before': updatedBefore.toString() }
+
+    const query = parsePageQuery(mockReq)
+    expect(query.created).to.equal(undefined)
+    expect(query.updated?.before?.getTime()).to.equal(updatedBefore)
+    expect(query.updated?.after).to.equal(undefined)
+    expect(query.revisions).to.equal(undefined)
+    expect(query.text).to.equal(undefined)
+    expect(query.limit).to.equal(undefined)
+    expect(query.offset).to.equal(undefined)
+    expect(query.sort).to.equal(undefined)
+  })
+
+  it('parses updated after', () => {
+    const updatedAfter = 1659571200000
+    mockReq.query = { 'updated-after': updatedAfter.toString() }
+
+    const query = parsePageQuery(mockReq)
+    expect(query.created).to.equal(undefined)
+    expect(query.updated?.before).to.equal(undefined)
+    expect(query.updated?.after?.getTime()).to.equal(updatedAfter)
+    expect(query.revisions).to.equal(undefined)
+    expect(query.text).to.equal(undefined)
+    expect(query.limit).to.equal(undefined)
+    expect(query.offset).to.equal(undefined)
+    expect(query.sort).to.equal(undefined)
+  })
+
+  it('parses both updated dates', () => {
+    const updatedBefore = 1659484800000
+    const updatedAfter = 1659571200000
+
+    mockReq.query = {
+      'updated-before': updatedBefore.toString(),
+      'updated-after': updatedAfter.toString()
+    }
+
+    const query = parsePageQuery(mockReq)
+    expect(query.created).to.equal(undefined)
+    expect(query.updated?.before?.getTime()).to.equal(updatedBefore)
+    expect(query.updated?.after?.getTime()).to.equal(updatedAfter)
+    expect(query.revisions).to.equal(undefined)
+    expect(query.text).to.equal(undefined)
+    expect(query.limit).to.equal(undefined)
+    expect(query.offset).to.equal(undefined)
+    expect(query.sort).to.equal(undefined)
+  })
+
+  it('parses all dates', () => {
+    const createdBefore = 1659312000000
+    const createdAfter = 1659398400000
+    const updatedBefore = 1659484800000
+    const updatedAfter = 1659571200000
+
+    mockReq.query = {
+      'created-before': createdBefore.toString(),
+      'created-after': createdAfter.toString(),
+      'updated-before': updatedBefore.toString(),
+      'updated-after': updatedAfter.toString()
+    }
+
+    const query = parsePageQuery(mockReq)
+    expect(query.created?.before?.getTime()).to.equal(createdBefore)
+    expect(query.created?.after?.getTime()).to.equal(createdAfter)
+    expect(query.updated?.before?.getTime()).to.equal(updatedBefore)
+    expect(query.updated?.after?.getTime()).to.equal(updatedAfter)
+    expect(query.revisions).to.equal(undefined)
+    expect(query.text).to.equal(undefined)
+    expect(query.limit).to.equal(undefined)
+    expect(query.offset).to.equal(undefined)
+    expect(query.sort).to.equal(undefined)
+  })
+
+  it('parses minimum revisions', () => {
+    const revisionsMin = 2
+    mockReq.query = { 'revisions-minimum': revisionsMin.toString() }
+
+    const query = parsePageQuery(mockReq)
+    expect(query.created).to.equal(undefined)
+    expect(query.created).to.equal(undefined)
+    expect(query.revisions?.min).to.equal(revisionsMin)
+    expect(query.revisions?.max).to.equal(undefined)
+    expect(query.text).to.equal(undefined)
+    expect(query.limit).to.equal(undefined)
+    expect(query.offset).to.equal(undefined)
+    expect(query.sort).to.equal(undefined)
+  })
+
+  it('parses maximum revisions', () => {
+    const revisionsMax = 20
+    mockReq.query = { 'revisions-maximum': revisionsMax.toString() }
+
+    const query = parsePageQuery(mockReq)
+    expect(query.created).to.equal(undefined)
+    expect(query.created).to.equal(undefined)
+    expect(query.revisions?.min).to.equal(undefined)
+    expect(query.revisions?.max).to.equal(revisionsMax)
+    expect(query.text).to.equal(undefined)
+    expect(query.limit).to.equal(undefined)
+    expect(query.offset).to.equal(undefined)
+    expect(query.sort).to.equal(undefined)
+  })
+
+  it('parses minimum and maximum revisions', () => {
+    const revisionsMin = 2
+    const revisionsMax = 20
+
+    mockReq.query = {
+      'revisions-minimum': revisionsMin.toString(),
+      'revisions-maximum': revisionsMax.toString()
+    }
+
+    const query = parsePageQuery(mockReq)
+    expect(query.created).to.equal(undefined)
+    expect(query.created).to.equal(undefined)
+    expect(query.revisions?.min).to.equal(revisionsMin)
+    expect(query.revisions?.max).to.equal(revisionsMax)
+    expect(query.text).to.equal(undefined)
+    expect(query.limit).to.equal(undefined)
+    expect(query.offset).to.equal(undefined)
+    expect(query.sort).to.equal(undefined)
+  })
+
+  it('parses text', () => {
+    const text = 'text search query'
+    mockReq.query = { text }
+
+    const query = parsePageQuery(mockReq)
+    expect(query.created).to.equal(undefined)
+    expect(query.created).to.equal(undefined)
+    expect(query.revisions).to.equal(undefined)
+    expect(query.text).to.equal(text)
+    expect(query.limit).to.equal(undefined)
+    expect(query.offset).to.equal(undefined)
+    expect(query.sort).to.equal(undefined)
+  })
+
+  it('parses limit', () => {
+    const limit = 50
+    mockReq.query = { limit: limit.toString() }
+
+    const query = parsePageQuery(mockReq)
+    expect(query.created).to.equal(undefined)
+    expect(query.created).to.equal(undefined)
+    expect(query.revisions).to.equal(undefined)
+    expect(query.text).to.equal(undefined)
+    expect(query.limit).to.equal(limit)
+    expect(query.offset).to.equal(undefined)
+    expect(query.sort).to.equal(undefined)
+  })
+
+  it('parses offset', () => {
+    const offset = 100
+    mockReq.query = { offset: offset.toString() }
+
+    const query = parsePageQuery(mockReq)
+    expect(query.created).to.equal(undefined)
+    expect(query.created).to.equal(undefined)
+    expect(query.revisions).to.equal(undefined)
+    expect(query.text).to.equal(undefined)
+    expect(query.limit).to.equal(undefined)
+    expect(query.offset).to.equal(offset)
+    expect(query.sort).to.equal(undefined)
+  })
+
+  it('parses a request to sort in order of created date', () => {
+    const sort = 'created'
+    mockReq.query = { sort }
+
+    const query = parsePageQuery(mockReq)
+    expect(query.created).to.equal(undefined)
+    expect(query.created).to.equal(undefined)
+    expect(query.revisions).to.equal(undefined)
+    expect(query.text).to.equal(undefined)
+    expect(query.limit).to.equal(undefined)
+    expect(query.offset).to.equal(undefined)
+    expect(query.sort).to.equal(sort)
+  })
+
+  it('parses a request to sort in reverse order of created date', () => {
+    const sort = '-created'
+    mockReq.query = { sort }
+
+    const query = parsePageQuery(mockReq)
+    expect(query.created).to.equal(undefined)
+    expect(query.created).to.equal(undefined)
+    expect(query.revisions).to.equal(undefined)
+    expect(query.text).to.equal(undefined)
+    expect(query.limit).to.equal(undefined)
+    expect(query.offset).to.equal(undefined)
+    expect(query.sort).to.equal(sort)
+  })
+
+  it('parses a request to sort in order of updated date', () => {
+    const sort = 'updated'
+    mockReq.query = { sort }
+
+    const query = parsePageQuery(mockReq)
+    expect(query.created).to.equal(undefined)
+    expect(query.created).to.equal(undefined)
+    expect(query.revisions).to.equal(undefined)
+    expect(query.text).to.equal(undefined)
+    expect(query.limit).to.equal(undefined)
+    expect(query.offset).to.equal(undefined)
+    expect(query.sort).to.equal(sort)
+  })
+
+  it('parses a request to sort in reverse order of updated date', () => {
+    const sort = '-updated'
+    mockReq.query = { sort }
+
+    const query = parsePageQuery(mockReq)
+    expect(query.created).to.equal(undefined)
+    expect(query.created).to.equal(undefined)
+    expect(query.revisions).to.equal(undefined)
+    expect(query.text).to.equal(undefined)
+    expect(query.limit).to.equal(undefined)
+    expect(query.offset).to.equal(undefined)
+    expect(query.sort).to.equal(sort)
+  })
+
+  it('parses a request to sort in alphabetical order by title', () => {
+    const sort = 'alphabetical'
+    mockReq.query = { sort }
+
+    const query = parsePageQuery(mockReq)
+    expect(query.created).to.equal(undefined)
+    expect(query.created).to.equal(undefined)
+    expect(query.revisions).to.equal(undefined)
+    expect(query.text).to.equal(undefined)
+    expect(query.limit).to.equal(undefined)
+    expect(query.offset).to.equal(undefined)
+    expect(query.sort).to.equal(sort)
+  })
+
+  it('parses a request to sort in reverse alphabetical order by title', () => {
+    const sort = '-alphabetical'
+    mockReq.query = { sort }
+
+    const query = parsePageQuery(mockReq)
+    expect(query.created).to.equal(undefined)
+    expect(query.created).to.equal(undefined)
+    expect(query.revisions).to.equal(undefined)
+    expect(query.text).to.equal(undefined)
+    expect(query.limit).to.equal(undefined)
+    expect(query.offset).to.equal(undefined)
+    expect(query.sort).to.equal(sort)
+  })
+
+  it('parses a request to sort in search relevance order', () => {
+    const sort = 'relevance'
+    mockReq.query = { sort }
+
+    const query = parsePageQuery(mockReq)
+    expect(query.created).to.equal(undefined)
+    expect(query.created).to.equal(undefined)
+    expect(query.revisions).to.equal(undefined)
+    expect(query.text).to.equal(undefined)
+    expect(query.limit).to.equal(undefined)
+    expect(query.offset).to.equal(undefined)
+    expect(query.sort).to.equal(sort)
+  })
+
+  it('rejects a request to sort in an unknown order', () => {
+    const sort = 'logarithmic'
+    mockReq.query = { sort }
+
+    const query = parsePageQuery(mockReq)
+    expect(query.created).to.equal(undefined)
+    expect(query.created).to.equal(undefined)
+    expect(query.revisions).to.equal(undefined)
+    expect(query.text).to.equal(undefined)
+    expect(query.limit).to.equal(undefined)
+    expect(query.offset).to.equal(undefined)
+    expect(query.sort).to.equal(undefined)
+  })
+
+  it('parses everything at once', () => {
+    const createdBefore = 1659312000000
+    const createdAfter = 1659398400000
+    const updatedBefore = 1659484800000
+    const updatedAfter = 1659571200000
+    const revisionsMin = 2
+    const revisionsMax = 20
+    const text = 'text search query'
+    const limit = 50
+    const offset = 100
+    const sort = 'relevance'
+
+    mockReq.query = {
+      'created-before': createdBefore.toString(),
+      'created-after': createdAfter.toString(),
+      'updated-before': updatedBefore.toString(),
+      'updated-after': updatedAfter.toString(),
+      'revisions-minimum': revisionsMin.toString(),
+      'revisions-maximum': revisionsMax.toString(),
+      text,
+      limit: limit.toString(),
+      offset: offset.toString(),
+      sort
+    }
+
+    const query = parsePageQuery(mockReq)
+    expect(query.created?.before?.getTime()).to.equal(createdBefore)
+    expect(query.created?.after?.getTime()).to.equal(createdAfter)
+    expect(query.updated?.before?.getTime()).to.equal(updatedBefore)
+    expect(query.updated?.after?.getTime()).to.equal(updatedAfter)
+    expect(query.revisions?.min).to.equal(revisionsMin)
+    expect(query.revisions?.max).to.equal(revisionsMax)
+    expect(query.text).to.equal(text)
+    expect(query.limit).to.equal(limit)
+    expect(query.offset).to.equal(offset)
+    expect(query.sort).to.equal(sort)
+  })
+})
---
+++ src/parse/page-query.ts
@@ -0,0 +1,63 @@
+import { Request } from 'express'
+import { PageQuery } from '../models/page/search/data.js'
+
+type PageQuerySortOption = 'created' | '-created' | 'updated' | '-updated' | 'alphabetical' | '-alphabetical' | 'relevance'
+type PageQueryNumericalField = 'created-before' | 'created-after' | 'updated-before' | 'updated-after' | 'revisions-minimum' | 'revisions-maximum' | 'limit' | 'offset'
+
+const parsePageQueryDates = (req: Request, field: 'created' | 'updated'): undefined | { before?: Date, after?: Date } => {
+  const before = parsePageQueryNumber(req, `${field}-before`)
+  const after = parsePageQueryNumber(req, `${field}-after`)
+  if (before === undefined && after === undefined) return undefined
+
+  const obj: { before?: Date, after?: Date } = {}
+  if (before !== undefined) obj.before = new Date(before)
+  if (after !== undefined) obj.after = new Date(after)
+  return obj
+}
+
+const parsePageQueryRevisions = (req: Request): undefined | { min?: number, max?: number } => {
+  const min = parsePageQueryNumber(req, 'revisions-minimum')
+  const max = parsePageQueryNumber(req, 'revisions-maximum')
+  if (min === undefined && max === undefined) return undefined
+
+  const obj: { min?: number, max?: number } = {}
+  if (min !== undefined) obj.min = min
+  if (max !== undefined) obj.max = max
+  return obj
+}
+
+const parsePageQueryNumber = (req: Request, field: PageQueryNumericalField): undefined | number => {
+  const num = parseInt(req.query[field] as string)
+  return isNaN(num) ? undefined : num
+}
+
+const parsePageQuerySort = (req: Request): undefined | PageQuerySortOption => {
+  const options = ['created', '-created', 'updated', '-updated', 'alphabetical', '-alphabetical', 'relevance']
+  if (options.includes(req.query.sort as string)) return req.query.sort as PageQuerySortOption
+  return undefined
+}
+
+const parsePageQuery = (req: Request): PageQuery => {
+  const query: PageQuery = {}
+
+  const created = parsePageQueryDates(req, 'created')
+  const updated = parsePageQueryDates(req, 'updated')
+  const revisions = parsePageQueryRevisions(req)
+  if (created !== undefined) query.created = created
+  if (updated !== undefined) query.updated = updated
+  if (revisions !== undefined) query.revisions = revisions
+
+  const limit = parsePageQueryNumber(req, 'limit')
+  const offset = parsePageQueryNumber(req, 'offset')
+  if (limit !== undefined) query.limit = limit
+  if (offset !== undefined) query.offset = offset
+
+  const sort = parsePageQuerySort(req)
+  if (sort !== undefined) query.sort = sort
+
+  if (req.query.text !== undefined) query.text = req.query.text as string
+
+  return query
+}
+
+export default parsePageQuery
