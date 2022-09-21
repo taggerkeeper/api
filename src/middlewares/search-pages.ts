44061fad9796a4550d3828ed5dfb11ca417adf48
Jason Godesky <jefgodesky@gmail.com>
2022-09-21 19:58:28 -0400

    Fix linting errors

--- src/middlewares/search-pages.ts
+++ src/middlewares/search-pages.ts
@@ -3,7 +3,7 @@ import expressAsyncHandler from 'express-async-handler'
 import search from '../models/page/search/search.js'
 import parsePageQuery from '../parse/page-query.js'
 
-const searchPages = async (req: Request, res: Response, next: NextFunction) => {
+const searchPages = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
   req.searchResults = await search(parsePageQuery(req), req.user)
   next()
 }
--- src/utils/get-query-str.ts
+++ src/utils/get-query-str.ts
@@ -2,6 +2,6 @@ interface QueryObject {
   [key: string]: string | number | boolean
 }
 
-const getQueryStr = (obj: QueryObject) => Object.keys(obj).map(key => `${key}=${encodeURIComponent(obj[key].toString())}`).join('&')
+const getQueryStr = (obj: QueryObject): string => Object.keys(obj).map(key => `${key}=${encodeURIComponent(obj[key].toString())}`).join('&')
 
 export default getQueryStr
