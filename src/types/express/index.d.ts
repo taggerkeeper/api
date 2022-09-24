dcd9b885503111ce1464835a2c484f0e7dd6614c
Jason Godesky <jefgodesky@gmail.com>
2022-09-24 16:29:04 -0400

    Express Request can have a Revision

--- src/types/express/index.d.ts
+++ src/types/express/index.d.ts
@@ -2,6 +2,7 @@ import * as sinon from 'sinon'
 import Email from '../../models/email/email.js'
 import User, { TokenSet } from '../../models/user/user.js'
 import { PageQueryResultSet } from '../../models/page/search/data'
+import Revision from '../../models/revision/revision.js'
 
 declare global {
   namespace Express {
@@ -11,7 +12,8 @@ declare global {
       email?: Email
       emailer?: Function | sinon.SinonStub
       tokens?: TokenSet
-      searchResults?: PageQueryResultSet
+      searchResults?: PageQueryResultSet,
+      revision?: Revision
     }
   }
 }
