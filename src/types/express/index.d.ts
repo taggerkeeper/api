b1a465e875575aef078f0462113634e0e0e2381b
Jason Godesky <jefgodesky@gmail.com>
2022-09-24 16:59:59 -0400

    Express Request can have a Page

--- src/types/express/index.d.ts
+++ src/types/express/index.d.ts
@@ -1,19 +1,21 @@
 import * as sinon from 'sinon'
 import Email from '../../models/email/email.js'
 import User, { TokenSet } from '../../models/user/user.js'
-import { PageQueryResultSet } from '../../models/page/search/data'
 import Revision from '../../models/revision/revision.js'
+import Page from '../../models/page/page.js'
+import { PageQueryResultSet } from '../../models/page/search/data'
 
 declare global {
   namespace Express {
     interface Request {
-      user?: User
-      subject?: User
       email?: Email
       emailer?: Function | sinon.SinonStub
-      tokens?: TokenSet
-      searchResults?: PageQueryResultSet
+      page?: Page
       revision?: Revision
+      searchResults?: PageQueryResultSet
+      subject?: User
+      tokens?: TokenSet
+      user?: User
     }
   }
 }
