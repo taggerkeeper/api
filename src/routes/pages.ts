28c7e79ef8446c80e81850749c6d35ef187d8390
Jason Godesky <jefgodesky@gmail.com>
2022-09-24 13:49:14 -0400

    GET /pages
    Plus HEAD /pages

--- src/routes/pages.ts
+++ src/routes/pages.ts
@@ -1,12 +1,87 @@
 import { Request, Response, Router } from 'express'
 
 import allow from '../middlewares/allow.js'
+import loadUserFromAccessToken from '../middlewares/load-user-from-access-token.js'
+import searchPages from '../middlewares/search-pages.js'
+import addSearchPagination from '../middlewares/add-search-pagination.js'
 
 const router = Router()
+
+/**
+ * @openapi
+ * components:
+ *   schemas:
+ *     Page:
+ *       type: object
+ *       description: "The data model that the API uses when returning page data."
+ *       properties:
+ *         id:
+ *           type: string
+ *           description: "The page's unique 24-digit hexadecimal ID number."
+ *           example: "0123456789abcdef12345678"
+ *         revisions:
+ *           type: array
+ *           items:
+ *             type: object
+ *             properties:
+ *               content:
+ *                 type: object
+ *                 properties:
+ *                   title:
+ *                     type: string
+ *                     description: "The page's title (as of this revision)."
+ *                     example: "Example Page"
+ *                   path:
+ *                     type: string
+ *                     description: "The page's unique path (as of this revision)."
+ *                     example: "/example-page"
+ *                   body:
+ *                     type: string
+ *                     description: "The content of the page (as of this revision)."
+ *                     example: "This page is an example."
+ *               permissions:
+ *                 type: object
+ *                 properties:
+ *                   read:
+ *                     type: string
+ *                     enum: [anyone, authenticated, editor, admin]
+ *                     description: "Read permissions for this page (as of this revision)."
+ *                   write:
+ *                     type: string
+ *                     enum: [anyone, authenticated, editor, admin]
+ *                     description: "Write permissions for this page (as of this revision)."
+ *               editor:
+ *                 description: "The user who made this revision."
+ *                 $ref: "#/components/schemas/User"
+ *               msg:
+ *                 type: string
+ *                 description: "A message explaining the purpose and intent of the revision."
+ *                 example: "Initial text"
+ *               timestamp:
+ *                 type: number
+ *                 description: "The timestamp of when the revision was made (measured as milliseconds since January 1, 1970)."
+ *                 example: 1663863528000
+ *           created:
+ *             type: number
+ *             description: "The timestamp of when this page was created (measured as milliseconds since January 1, 1970)."
+ *             example: 1663863528000
+ *           updated:
+ *             type: number
+ *             description: "The timestamp of when this page was most recently updated (measured as milliseconds since January 1, 1970)."
+ *             example: 1663863528000
+ */
+
+// /pages
 
 const collection = {
   options: (req: Request, res: Response) => {
     res.sendStatus(204)
+  },
+  head: (req: Request, res: Response) => {
+    res.sendStatus(204)
+  },
+  get: (req: Request, res: Response) => {
+    res.status(200).send(req.searchResults)
   }
 }
 
@@ -36,5 +111,210 @@ router.all('/', allow(collection))
  */
 
 router.options('/', collection.options)
+
+/**
+ * @openapi
+ * /pages:
+ *   head:
+ *     summary: "Headers for search."
+ *     description: "This endpoint allows users to run a search, but only return the headers for it."
+ *     tags:
+ *       - pages
+ *     parameters:
+ *       - in: query
+ *         name: created-before
+ *         schema:
+ *           type: number
+ *         description: "Limit results to only those pages created before this time (measured as milliseconds since January 1, 1970)."
+ *         example: 1663863528000
+ *       - in: query
+ *         name: created-after
+ *         schema:
+ *           type: number
+ *         description: "Limit results to only those pages created after this time (measured as milliseconds since January 1, 1970)."
+ *         example: 1663863528000
+ *       - in: query
+ *         name: updated-before
+ *         schema:
+ *           type: number
+ *         description: "Limit results to only those pages most recently updated before this time (measured as milliseconds since January 1, 1970)."
+ *         example: 1663863528000
+ *       - in: query
+ *         name: updated-after
+ *         schema:
+ *           type: number
+ *         description: "Limit results to only those pages most recently updated after this time (measured as milliseconds since January 1, 1970)."
+ *         example: 1663863528000
+ *       - in: query
+ *         name: revisions-minimum
+ *         schema:
+ *           type: number
+ *         description: "Limit results to only those pages that have this many revisions or more."
+ *         example: 1
+ *       - in: query
+ *         name: revisions-maximum
+ *         schema:
+ *           type: number
+ *         description: "Limit results to only those pages that have this many revisions or fewer."
+ *         example: 10
+ *       - in: query
+ *         name: limit
+ *         schema:
+ *           type: number
+ *         description: "Return no more than this many pages in each response."
+ *         example: 50
+ *       - in: query
+ *         name: offset
+ *         schema:
+ *           type: number
+ *         description: "Offset the returned set by this many (e.g., start the returned set at this index)."
+ *         example: 150
+ *       - in: query
+ *         name: sort
+ *         schema:
+ *           type: string
+ *           enum: [created, -created, updated, -updated, alphabetical, -alphabetical, relevance]
+ *         description: "Specifies how the results should be sorted. Options that begin with a minus sign are inverted (e.g., `-created` means in reverse chronological order by time when the page was created). The `relevance` option only works when you also provide a `text` parameter to search for."
+ *         example: alphabetical
+ *       - in: query
+ *         name: text
+ *         schema:
+ *           type: string
+ *         description: "A search string."
+ *         example: Poughkeepsie
+ *     responses:
+ *       204:
+ *         headers:
+ *           'Allow':
+ *             schema:
+ *               type: string
+ *               example: "OPTIONS"
+ *             description: "The methods that this endpoint allows."
+ *           'Access-Control-Allow-Methods':
+ *             schema:
+ *               type: string
+ *               example: "OPTIONS"
+ *             description: "The methods that this endpoint allows."
+ *           'Link':
+ *             schema:
+ *               type: string
+ *               example: <https://taggerkeeper.com/v1/pages?text=example&offset=0&limit=50>; rel="first"
+ *             description: "Links to the first, previous, next, and last pages in the search set, as appropriate."
+ */
+
+router.head('/', loadUserFromAccessToken, searchPages, addSearchPagination, collection.head)
+
+/**
+ * @openapi
+ * /pages:
+ *   get:
+ *     summary: "Headers for search."
+ *     description: "This endpoint allows users to run a search, but only return the headers for it."
+ *     tags:
+ *       - pages
+ *     parameters:
+ *       - in: query
+ *         name: created-before
+ *         schema:
+ *           type: number
+ *         description: "Limit results to only those pages created before this time (measured as milliseconds since January 1, 1970)."
+ *         example: 1663863528000
+ *       - in: query
+ *         name: created-after
+ *         schema:
+ *           type: number
+ *         description: "Limit results to only those pages created after this time (measured as milliseconds since January 1, 1970)."
+ *         example: 1663863528000
+ *       - in: query
+ *         name: updated-before
+ *         schema:
+ *           type: number
+ *         description: "Limit results to only those pages most recently updated before this time (measured as milliseconds since January 1, 1970)."
+ *         example: 1663863528000
+ *       - in: query
+ *         name: updated-after
+ *         schema:
+ *           type: number
+ *         description: "Limit results to only those pages most recently updated after this time (measured as milliseconds since January 1, 1970)."
+ *         example: 1663863528000
+ *       - in: query
+ *         name: revisions-minimum
+ *         schema:
+ *           type: number
+ *         description: "Limit results to only those pages that have this many revisions or more."
+ *         example: 1
+ *       - in: query
+ *         name: revisions-maximum
+ *         schema:
+ *           type: number
+ *         description: "Limit results to only those pages that have this many revisions or fewer."
+ *         example: 10
+ *       - in: query
+ *         name: limit
+ *         schema:
+ *           type: number
+ *         description: "Return no more than this many pages in each response."
+ *         example: 50
+ *       - in: query
+ *         name: offset
+ *         schema:
+ *           type: number
+ *         description: "Offset the returned set by this many (e.g., start the returned set at this index)."
+ *         example: 150
+ *       - in: query
+ *         name: sort
+ *         schema:
+ *           type: string
+ *           enum: [created, -created, updated, -updated, alphabetical, -alphabetical, relevance]
+ *         description: "Specifies how the results should be sorted. Options that begin with a minus sign are inverted (e.g., `-created` means in reverse chronological order by time when the page was created). The `relevance` option only works when you also provide a `text` parameter to search for."
+ *         example: alphabetical
+ *       - in: query
+ *         name: text
+ *         schema:
+ *           type: string
+ *         description: "A search string."
+ *         example: Poughkeepsie
+ *     responses:
+ *       200:
+ *         headers:
+ *           'Allow':
+ *             schema:
+ *               type: string
+ *               example: "OPTIONS"
+ *             description: "The methods that this endpoint allows."
+ *           'Access-Control-Allow-Methods':
+ *             schema:
+ *               type: string
+ *               example: "OPTIONS"
+ *             description: "The methods that this endpoint allows."
+ *           'Link':
+ *             schema:
+ *               type: string
+ *               example: <https://taggerkeeper.com/v1/pages?text=example&offset=0&limit=50>; rel="first"
+ *             description: "Links to the first, previous, next, and last pages in the search set, as appropriate."
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 total:
+ *                   type: number
+ *                   description: "The total number of pages found by your query."
+ *                   example: 1000
+ *                 start:
+ *                   type: number
+ *                   description: "The index of the first page in the current set in the total set."
+ *                   example: 0
+ *                 end:
+ *                   type: number
+ *                   description: "The index of the last page in the current set in the total set."
+ *                   example: 50
+ *                 pages:
+ *                   type: array
+ *                   items:
+ *                     $ref: "#/components/schemas/Page"
+ */
+
+router.get('/', loadUserFromAccessToken, searchPages, addSearchPagination, collection.get)
 
 export default router
--- src/routes/tests/pages.ts
+++ src/routes/tests/pages.ts
@@ -2,13 +2,72 @@ import { expect } from 'chai'
 import request from 'supertest'
 import loadPackage, { NPMPackage } from '../../utils/load-package.js'
 import getAPIInfo from '../../utils/get-api-info.js'
+import { PermissionLevel } from '../../models/permissions/data.js'
+import Page from '../../models/page/page.js'
+import User from '../../models/user/user.js'
 import api from '../../server.js'
+
+const parseLinks = (header: string): any => {
+  const links: any = {}
+  const coll = header.split(',').map(link => link.trim())
+  coll.forEach(link => {
+    const match = link.match(/<(.*?)>; rel="(.*?)"/i)
+    if (match !== null && match.length > 2) {
+      links[match[2]] = match[1]
+    }
+  })
+  return links
+}
+
+const runSearchTest = async (base: string, method: 'head' | 'get' = 'get'): Promise<any> => {
+  const users = {
+    authenticated: new User(),
+    editor: new User(),
+    admin: new User({ name: 'Admin', admin: true })
+  }
+
+  await users.authenticated.save()
+  await users.editor.save()
+  await users.admin.save()
+
+  const tokens = {
+    authenticated: await users.authenticated.generateTokens(),
+    editor: await users.editor.generateTokens(),
+    admin: await users.admin.generateTokens()
+  }
+
+  await users.authenticated.save()
+  await users.editor.save()
+  await users.admin.save()
+
+  for (let i = 1; i <= 10; i++) {
+    const page = new Page({ revisions: [{ content: { title: `Test Page #${i}`, path: `/test-${i}`, body: 'This is a test page.' } }] })
+    await page.save()
+  }
+
+  const authOnly = new Page({ revisions: [{ content: { title: 'Authenticated Only', path: '/auth', body: 'Authenticated only.' }, permissions: { read: PermissionLevel.authenticated, write: PermissionLevel.authenticated } }] })
+  const editorOnly = new Page({ revisions: [{ content: { title: 'Editor Only', path: '/editor', body: 'Editor only.' }, permissions: { read: PermissionLevel.editor, write: PermissionLevel.editor }, editor: users.editor.getObj() }] })
+  const adminOnly = new Page({ revisions: [{ content: { title: 'Admin Only', path: '/admin', body: 'Admin only.' }, permissions: { read: PermissionLevel.admin, write: PermissionLevel.admin } }] })
+
+  await authOnly.save()
+  await editorOnly.save()
+  await adminOnly.save()
+
+  const url = `${base}/pages?created-after=0&offset=4&limit=2`
+  const fn = method === 'head' ? request(api).head : request(api).get
+  return {
+    anonymous: await fn(url),
+    authenticated: await fn(url).set({ Authorization: `Bearer ${tokens.authenticated.access}` }),
+    editor: await fn(url).set({ Authorization: `Bearer ${tokens.editor.access}` }),
+    admin: await fn(url).set({ Authorization: `Bearer ${tokens.admin.access}` })
+  }
+}
 
 describe('Pages API', () => {
   let pkg: NPMPackage
   let base: string
   let res: any
-  const allow = 'OPTIONS'
+  const allow = 'OPTIONS, HEAD, GET'
 
   beforeEach(async () => {
     pkg = await loadPackage() as NPMPackage
@@ -17,6 +76,8 @@ describe('Pages API', () => {
   })
 
   describe('/pages', () => {
+    let results: any
+
     describe('OPTIONS /pages', () => {
       beforeEach(async () => {
         res = await request(api).options(`${base}/pages`)
@@ -32,6 +93,262 @@ describe('Pages API', () => {
 
       it('returns Access-Control-Allow-Methods header', () => {
         expect(res.headers['access-control-allow-methods']).to.equal(allow)
+      })
+    })
+
+    describe('HEAD /pages', () => {
+      beforeEach(async () => {
+        results = await runSearchTest(base, 'head')
+      })
+
+      describe('Anonymous calls', () => {
+        it('returns 204', () => {
+          expect(results.anonymous.status).to.equal(204)
+        })
+
+        it('returns Allow header', () => {
+          expect(results.anonymous.headers.allow).to.equal(allow)
+        })
+
+        it('returns Access-Control-Allow-Methods header', () => {
+          expect(results.anonymous.headers['access-control-allow-methods']).to.equal(allow)
+        })
+
+        it('returns link headers', () => {
+          const rels = Object.keys(parseLinks(results.anonymous.headers.link))
+          expect(rels).to.include('first')
+          expect(rels).to.include('previous')
+          expect(rels).to.include('next')
+          expect(rels).to.include('last')
+          expect(rels).to.have.lengthOf(4)
+        })
+
+        it('returns X-Total-Count header', () => {
+          expect(results.anonymous.headers['x-total-count']).to.equal('10')
+        })
+      })
+
+      describe('Authenticated calls', () => {
+        it('returns 204', () => {
+          expect(results.authenticated.status).to.equal(204)
+        })
+
+        it('returns Allow header', () => {
+          expect(results.authenticated.headers.allow).to.equal(allow)
+        })
+
+        it('returns Access-Control-Allow-Methods header', () => {
+          expect(results.authenticated.headers['access-control-allow-methods']).to.equal(allow)
+        })
+
+        it('returns link headers', () => {
+          const rels = Object.keys(parseLinks(results.authenticated.headers.link))
+          expect(rels).to.include('first')
+          expect(rels).to.include('previous')
+          expect(rels).to.include('next')
+          expect(rels).to.include('last')
+          expect(rels).to.have.lengthOf(4)
+        })
+
+        it('returns X-Total-Count header', () => {
+          expect(results.authenticated.headers['x-total-count']).to.equal('11')
+        })
+      })
+
+      describe('Editor calls', () => {
+        it('returns 204', () => {
+          expect(results.editor.status).to.equal(204)
+        })
+
+        it('returns Allow header', () => {
+          expect(results.editor.headers.allow).to.equal(allow)
+        })
+
+        it('returns Access-Control-Allow-Methods header', () => {
+          expect(results.editor.headers['access-control-allow-methods']).to.equal(allow)
+        })
+
+        it('returns link headers', () => {
+          const rels = Object.keys(parseLinks(results.editor.headers.link))
+          expect(rels).to.include('first')
+          expect(rels).to.include('previous')
+          expect(rels).to.include('next')
+          expect(rels).to.include('last')
+          expect(rels).to.have.lengthOf(4)
+        })
+
+        it('returns X-Total-Count header', () => {
+          expect(results.editor.headers['x-total-count']).to.equal('12')
+        })
+      })
+
+      describe('Admin calls', () => {
+        it('returns 204', () => {
+          expect(results.admin.status).to.equal(204)
+        })
+
+        it('returns Allow header', () => {
+          expect(results.admin.headers.allow).to.equal(allow)
+        })
+
+        it('returns Access-Control-Allow-Methods header', () => {
+          expect(results.admin.headers['access-control-allow-methods']).to.equal(allow)
+        })
+
+        it('returns link headers', () => {
+          const rels = Object.keys(parseLinks(results.admin.headers.link))
+          expect(rels).to.include('first')
+          expect(rels).to.include('previous')
+          expect(rels).to.include('next')
+          expect(rels).to.include('last')
+          expect(rels).to.have.lengthOf(4)
+        })
+
+        it('returns X-Total-Count header', () => {
+          expect(results.admin.headers['x-total-count']).to.equal('13')
+        })
+      })
+    })
+
+    describe('GET /pages', () => {
+      beforeEach(async () => {
+        results = await runSearchTest(base)
+      })
+
+      describe('Anonymous calls', () => {
+        it('returns 200', () => {
+          expect(results.anonymous.status).to.equal(200)
+        })
+
+        it('returns Allow header', () => {
+          expect(results.anonymous.headers.allow).to.equal(allow)
+        })
+
+        it('returns Access-Control-Allow-Methods header', () => {
+          expect(results.anonymous.headers['access-control-allow-methods']).to.equal(allow)
+        })
+
+        it('returns link headers', () => {
+          const rels = Object.keys(parseLinks(results.anonymous.headers.link))
+          expect(rels).to.include('first')
+          expect(rels).to.include('previous')
+          expect(rels).to.include('next')
+          expect(rels).to.include('last')
+          expect(rels).to.have.lengthOf(4)
+        })
+
+        it('returns X-Total-Count header', () => {
+          expect(results.anonymous.headers['x-total-count']).to.equal('10')
+        })
+
+        it('returns your results', () => {
+          expect(results.anonymous.body.total).to.equal(10)
+          expect(results.anonymous.body.start).to.equal(4)
+          expect(results.anonymous.body.end).to.equal(6)
+          expect(results.anonymous.body.pages).to.have.lengthOf(2)
+        })
+      })
+
+      describe('Authenticated calls', () => {
+        it('returns 200', () => {
+          expect(results.authenticated.status).to.equal(200)
+        })
+
+        it('returns Allow header', () => {
+          expect(results.authenticated.headers.allow).to.equal(allow)
+        })
+
+        it('returns Access-Control-Allow-Methods header', () => {
+          expect(results.authenticated.headers['access-control-allow-methods']).to.equal(allow)
+        })
+
+        it('returns link headers', () => {
+          const rels = Object.keys(parseLinks(results.authenticated.headers.link))
+          expect(rels).to.include('first')
+          expect(rels).to.include('previous')
+          expect(rels).to.include('next')
+          expect(rels).to.include('last')
+          expect(rels).to.have.lengthOf(4)
+        })
+
+        it('returns X-Total-Count header', () => {
+          expect(results.authenticated.headers['x-total-count']).to.equal('11')
+        })
+
+        it('returns your results', () => {
+          expect(results.authenticated.body.total).to.equal(11)
+          expect(results.authenticated.body.start).to.equal(4)
+          expect(results.authenticated.body.end).to.equal(6)
+          expect(results.authenticated.body.pages).to.have.lengthOf(2)
+        })
+      })
+
+      describe('Editor calls', () => {
+        it('returns 200', () => {
+          expect(results.editor.status).to.equal(200)
+        })
+
+        it('returns Allow header', () => {
+          expect(results.editor.headers.allow).to.equal(allow)
+        })
+
+        it('returns Access-Control-Allow-Methods header', () => {
+          expect(results.editor.headers['access-control-allow-methods']).to.equal(allow)
+        })
+
+        it('returns link headers', () => {
+          const rels = Object.keys(parseLinks(results.editor.headers.link))
+          expect(rels).to.include('first')
+          expect(rels).to.include('previous')
+          expect(rels).to.include('next')
+          expect(rels).to.include('last')
+          expect(rels).to.have.lengthOf(4)
+        })
+
+        it('returns X-Total-Count header', () => {
+          expect(results.editor.headers['x-total-count']).to.equal('12')
+        })
+
+        it('returns your results', () => {
+          expect(results.editor.body.total).to.equal(12)
+          expect(results.editor.body.start).to.equal(4)
+          expect(results.editor.body.end).to.equal(6)
+          expect(results.editor.body.pages).to.have.lengthOf(2)
+        })
+      })
+
+      describe('Admin calls', () => {
+        it('returns 200', () => {
+          expect(results.admin.status).to.equal(200)
+        })
+
+        it('returns Allow header', () => {
+          expect(results.admin.headers.allow).to.equal(allow)
+        })
+
+        it('returns Access-Control-Allow-Methods header', () => {
+          expect(results.admin.headers['access-control-allow-methods']).to.equal(allow)
+        })
+
+        it('returns link headers', () => {
+          const rels = Object.keys(parseLinks(results.admin.headers.link))
+          expect(rels).to.include('first')
+          expect(rels).to.include('previous')
+          expect(rels).to.include('next')
+          expect(rels).to.include('last')
+          expect(rels).to.have.lengthOf(4)
+        })
+
+        it('returns X-Total-Count header', () => {
+          expect(results.admin.headers['x-total-count']).to.equal('13')
+        })
+
+        it('returns your results', () => {
+          expect(results.admin.body.total).to.equal(13)
+          expect(results.admin.body.start).to.equal(4)
+          expect(results.admin.body.end).to.equal(6)
+          expect(results.admin.body.pages).to.have.lengthOf(2)
+        })
       })
     })
   })
