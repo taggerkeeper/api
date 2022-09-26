78ccc9e55621ad5eed0c47506204ba93ec0c5709
Jason Godesky <jefgodesky@gmail.com>
2022-09-26 15:07:34 -0400

    Loader: loadPageByPath: Incorporate security subquery

--- src/models/page/loaders/by-path.ts
+++ src/models/page/loaders/by-path.ts
@@ -1,9 +1,11 @@
+import getSecuritySubquery from '../search/get-security-subquery.js'
 import Page from '../page.js'
 import PageModel from '../model.js'
+import User from '../../user/user.js'
 
-const loadPageByPath = async (path: string): Promise<Page | null> => {
+const loadPageByPath = async (path: string, searcher?: User): Promise<Page | null> => {
   try {
-    const record = await PageModel.findOne({ 'revisions.0.content.path': path })
+    const record = await PageModel.findOne({ $and: [{ 'revisions.0.content.path': path }, getSecuritySubquery(searcher)] })
     return record === null ? null : new Page(record)
   } catch (err) {
     console.error(err)
