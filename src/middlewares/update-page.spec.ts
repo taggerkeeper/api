3c9d4706f352103e7a875eac4f4e10c0dd91803a
Jason Godesky <jefgodesky@gmail.com>
2022-09-26 19:25:41 -0400

    Middlewares: updatePage: Wrap permissions check into middleware

--- src/middlewares/update-page.spec.ts
+++ src/middlewares/update-page.spec.ts
@@ -1,10 +1,13 @@
-import { expect } from 'chai'
+import chai, { expect } from 'chai'
+import sinonChai from 'sinon-chai'
 import { mockRequest, mockResponse } from 'mock-req-res'
 import { PermissionLevel } from '../models/permissions/data.js'
 import User from '../models/user/user.js'
 import Revision from '../models/revision/revision.js'
 import Page from '../models/page/page.js'
 import updatePage from './update-page.js'
+
+chai.use(sinonChai)
 
 describe('updatePage', () => {
   let mockReq = mockRequest()
@@ -14,33 +17,181 @@ describe('updatePage', () => {
   const name = 'Tester'
   const uid = '0123456789abcdef12345678'
   const editor = new User({ id: uid, name })
-  const page = new Page({ revisions: [{ content: { title: 'Test Page', body: 'This is a test page.' } }] })
+  const other = new User()
+  const admin = new User({ id: 'abcdefabcdefabcdefabcdef', name: 'Admin', admin: true })
 
   const title = 'New Revision'
   const body = 'This is a new revision.'
   const msg = 'This is a commit message.'
+  const content = { title, body }
+  const revisions = {
+    anon: { content, permissions: { read: PermissionLevel.anyone, write: PermissionLevel.anyone }, editor: editor.getObj(), msg },
+    auth: { content, permissions: { read: PermissionLevel.anyone, write: PermissionLevel.authenticated }, editor: editor.getObj(), msg },
+    editor: { content, permissions: { read: PermissionLevel.anyone, write: PermissionLevel.editor }, editor: editor.getObj(), msg },
+    admin: { content, permissions: { read: PermissionLevel.anyone, write: PermissionLevel.admin }, editor: editor.getObj(), msg }
+  }
+  const pages = {
+    anon: new Page({ revisions: [revisions.anon] }),
+    auth: new Page({ revisions: [revisions.auth] }),
+    editor: new Page({ revisions: [revisions.editor] }),
+    admin: new Page({ revisions: [revisions.admin] })
+  }
+  const revision = new Revision({ content: { title, body }, editor: editor.getObj(), msg })
 
   beforeEach(() => {
     mockReq = mockRequest()
     mockRes = mockResponse()
     mockNext = () => {}
-    mockReq.page = page
-    mockReq.user = editor
-    mockReq.revision = new Revision({ content: { title, body }, editor: editor.getObj(), msg })
-    updatePage(mockReq, mockRes, mockNext)
+    mockReq.revision = revision
+    pages.anon = new Page({ revisions: [revisions.anon] })
+    pages.auth = new Page({ revisions: [revisions.auth] })
+    pages.editor = new Page({ revisions: [revisions.editor] })
+    pages.admin = new Page({ revisions: [revisions.admin] })
   })
 
-  it('updates the page', () => {
-    const { page } = mockReq
-    const revision = page?.revisions[0]
-    expect(page).not.to.equal(undefined)
-    expect(page?.revisions).to.have.lengthOf(2)
-    expect(revision?.content.title).to.equal(title)
-    expect(revision?.content.path).to.equal('/new-revision')
-    expect(revision?.content.body).to.equal(body)
-    expect(revision?.permissions.read).to.equal(PermissionLevel.anyone)
-    expect(revision?.permissions.write).to.equal(PermissionLevel.anyone)
-    expect(revision?.editor?.id).to.equal(uid)
-    expect(revision?.msg).to.equal(msg)
+  describe('Updating content', () => {
+    it('updates the content', () => {
+      mockReq.page = pages.anon
+      updatePage(mockReq, mockRes, mockNext)
+      expect(mockReq.page.revisions).to.have.lengthOf(2)
+      expect(mockReq.page.revisions[0].content.title).to.equal(title)
+      expect(mockReq.page.revisions[0].content.path).to.equal('/new-revision')
+      expect(mockReq.page.revisions[0].content.body).to.equal(body)
+      expect(mockReq.page.revisions[0].permissions.read).to.equal(PermissionLevel.anyone)
+      expect(mockReq.page.revisions[0].permissions.write).to.equal(PermissionLevel.anyone)
+      expect(mockReq.page.revisions[0].editor?.id).to.equal(uid)
+      expect(mockReq.page.revisions[0].msg).to.equal(msg)
+    })
+  })
+
+  describe('Pages that anyone can update', () => {
+    beforeEach(() => {
+      mockReq.page = pages.anon
+    })
+
+    it('lets anonymous users update', () => {
+      updatePage(mockReq, mockRes, mockNext)
+      expect(mockReq.page?.revisions).to.have.lengthOf(2)
+    })
+
+    it('lets authenticated users update', () => {
+      mockReq.user = other
+      updatePage(mockReq, mockRes, mockNext)
+      expect(mockReq.page?.revisions).to.have.lengthOf(2)
+    })
+
+    it('lets editors update', () => {
+      mockReq.user = editor
+      updatePage(mockReq, mockRes, mockNext)
+      expect(mockReq.page?.revisions).to.have.lengthOf(2)
+    })
+
+    it('lets admins update', () => {
+      mockReq.user = admin
+      updatePage(mockReq, mockRes, mockNext)
+      expect(mockReq.page?.revisions).to.have.lengthOf(2)
+    })
+  })
+
+  describe('Pages that only authenticated users can update', () => {
+    beforeEach(() => {
+      mockReq.page = pages.auth
+    })
+
+    it('doesn\'t let anonymous users update', () => {
+      updatePage(mockReq, mockRes, mockNext)
+      expect(mockReq.page?.revisions).to.have.lengthOf(1)
+      expect(mockRes.status).to.have.been.calledWith(401)
+      expect(mockRes.send).to.have.been.calledWithMatch({ message: 'This method requires authentication.' })
+      expect(mockRes.set).to.have.been.calledWith('WWW-Authenticate', 'Bearer error="invalid_token" error_description="The access token could not be verified."')
+    })
+
+    it('lets authenticated users update', () => {
+      mockReq.user = other
+      updatePage(mockReq, mockRes, mockNext)
+      expect(mockReq.page?.revisions).to.have.lengthOf(2)
+    })
+
+    it('lets editors update', () => {
+      mockReq.user = editor
+      updatePage(mockReq, mockRes, mockNext)
+      expect(mockReq.page?.revisions).to.have.lengthOf(2)
+    })
+
+    it('lets admins update', () => {
+      mockReq.user = admin
+      updatePage(mockReq, mockRes, mockNext)
+      expect(mockReq.page?.revisions).to.have.lengthOf(2)
+    })
+  })
+
+  describe('Pages that only editors can update', () => {
+    beforeEach(() => {
+      mockReq.page = pages.editor
+    })
+
+    it('doesn\'t let anonymous users update', () => {
+      updatePage(mockReq, mockRes, mockNext)
+      expect(mockReq.page?.revisions).to.have.lengthOf(1)
+      expect(mockRes.status).to.have.been.calledWith(401)
+      expect(mockRes.send).to.have.been.calledWithMatch({ message: 'This method requires authentication.' })
+      expect(mockRes.set).to.have.been.calledWith('WWW-Authenticate', 'Bearer error="invalid_token" error_description="The access token could not be verified."')
+    })
+
+    it('doesn\'t let authenticated users update', () => {
+      mockReq.user = other
+      updatePage(mockReq, mockRes, mockNext)
+      expect(mockReq.page?.revisions).to.have.lengthOf(1)
+      expect(mockRes.status).to.have.been.calledWith(403)
+      expect(mockRes.send).to.have.been.calledWithMatch({ message: 'You do not have permission to update this page.' })
+    })
+
+    it('lets editors update', () => {
+      mockReq.user = editor
+      updatePage(mockReq, mockRes, mockNext)
+      expect(mockReq.page?.revisions).to.have.lengthOf(2)
+    })
+
+    it('lets admins update', () => {
+      mockReq.user = admin
+      updatePage(mockReq, mockRes, mockNext)
+      expect(mockReq.page?.revisions).to.have.lengthOf(2)
+    })
+  })
+
+  describe('Pages that only admins can update', () => {
+    beforeEach(() => {
+      mockReq.page = pages.admin
+    })
+
+    it('doesn\'t let anonymous users update', () => {
+      updatePage(mockReq, mockRes, mockNext)
+      expect(mockReq.page?.revisions).to.have.lengthOf(1)
+      expect(mockRes.status).to.have.been.calledWith(401)
+      expect(mockRes.send).to.have.been.calledWithMatch({ message: 'This method requires authentication.' })
+      expect(mockRes.set).to.have.been.calledWith('WWW-Authenticate', 'Bearer error="invalid_token" error_description="The access token could not be verified."')
+    })
+
+    it('doesn\'t let authenticated users update', () => {
+      mockReq.user = other
+      updatePage(mockReq, mockRes, mockNext)
+      expect(mockReq.page?.revisions).to.have.lengthOf(1)
+      expect(mockRes.status).to.have.been.calledWith(403)
+      expect(mockRes.send).to.have.been.calledWithMatch({ message: 'You do not have permission to update this page.' })
+    })
+
+    it('doesn\'t let editors update', () => {
+      mockReq.user = editor
+      updatePage(mockReq, mockRes, mockNext)
+      expect(mockReq.page?.revisions).to.have.lengthOf(1)
+      expect(mockRes.status).to.have.been.calledWith(403)
+      expect(mockRes.send).to.have.been.calledWithMatch({ message: 'You do not have permission to update this page.' })
+    })
+
+    it('lets admins update', () => {
+      mockReq.user = admin
+      updatePage(mockReq, mockRes, mockNext)
+      expect(mockReq.page?.revisions).to.have.lengthOf(2)
+    })
   })
 })
--- src/middlewares/update-page.ts
+++ src/middlewares/update-page.ts
@@ -2,8 +2,16 @@ import { Request, Response, NextFunction } from 'express'
 
 const updatePage = function (req: Request, res: Response, next: NextFunction): void {
   const { page, revision } = req
-  if (revision !== undefined && page !== undefined) page.addRevision(revision)
-  next()
+  const permissionGranted = page?.revisions[0].permissions.canWrite(req.user, page.revisions)
+  if (permissionGranted !== true && req.user === undefined) {
+    res.set('WWW-Authenticate', 'Bearer error="invalid_token" error_description="The access token could not be verified."')
+    res.status(401).send({ message: 'This method requires authentication.' })
+  } else if (permissionGranted !== true) {
+    res.status(403).send({ message: 'You do not have permission to update this page.' })
+  } else {
+    if (revision !== undefined) page?.addRevision(revision)
+    next()
+  }
 }
 
 export default updatePage
