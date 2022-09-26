6dff1e224a748363f1a456f8d3007c175601ca3b
Jason Godesky <jefgodesky@gmail.com>
2022-09-26 17:05:37 -0400

    Middlewares: loadPage: Fix logic in picking loader
    The old logic would catch most paths in a flat wiki as attempted ID's. This tests to see if they're valid ID's, and if so, uses loadPageById; else, it tries loadPageByPath.

--- src/middlewares/load-page.ts
+++ src/middlewares/load-page.ts
@@ -1,14 +1,16 @@
+import mongoose from 'mongoose'
 import { Request, Response, NextFunction } from 'express'
 import expressAsyncHandler from 'express-async-handler'
 import getPath from '../utils/get-path.js'
 import loadPageById from '../models/page/loaders/by-id.js'
 import loadPageByPath from '../models/page/loaders/by-path.js'
+const { isValid } = mongoose.Types.ObjectId
 
 const loadPage = async function (req: Request, res: Response, next: NextFunction): Promise<void> {
   try {
     const { pid } = req.params
     const path = await getPath(req.originalUrl)
-    const page = path !== `/${pid}` ? await loadPageByPath(path, req.user) : await loadPageById(pid, req.user)
+    const page = isValid(pid) ? await loadPageById(pid, req.user) : await loadPageByPath(path, req.user)
     if (page !== null) req.page = page
   } catch (err) {
     console.error(err)
