523daa9718ecb7dba53b1e6e08579abaf91f78e5
Jason Godesky <jefgodesky@gmail.com>
2022-10-05 16:55:03 -0400

    Middlewares: requireRevisionIndex: Refactor to Page.getRevisionFromStr

--- src/middlewares/require-revision-index.spec.ts
+++ src/middlewares/require-revision-index.spec.ts
@@ -34,7 +34,7 @@ describe('requireRevisionIndex', () => {
     mockReq.page = new Page({ revisions })
     requireRevisionIndex(mockReq, mockRes, mockNext)
     expect(mockRes.status).to.have.been.calledWith(400)
-    expect(mockRes.send).to.have.been.calledWithMatch({ message: 'undefined is not a valid index for any revision of this page. Please provide an index between 0 and 0.' })
+    expect(mockRes.send).to.have.been.calledWithMatch({ message: 'undefined is not a valid index for any revision of this page. Please provide an index between 1 and 1.' })
     expect(mockReq.revision).to.equal(undefined)
     expect(mockNext).to.have.callCount(0)
   })
@@ -44,34 +44,34 @@ describe('requireRevisionIndex', () => {
     mockReq.params = { revision: 'nope' }
     requireRevisionIndex(mockReq, mockRes, mockNext)
     expect(mockRes.status).to.have.been.calledWith(400)
-    expect(mockRes.send).to.have.been.calledWithMatch({ message: 'nope is not a valid index for any revision of this page. Please provide an index between 0 and 0.' })
+    expect(mockRes.send).to.have.been.calledWithMatch({ message: 'nope is not a valid index for any revision of this page. Please provide an index between 1 and 1.' })
     expect(mockReq.revision).to.equal(undefined)
     expect(mockNext).to.have.callCount(0)
   })
 
-  it('returns 400 if given a number less than 0 for a revision', async () => {
+  it('returns 400 if given a number less than 1 for a revision', async () => {
     mockReq.page = new Page({ revisions })
-    mockReq.params = { revision: '-1' }
+    mockReq.params = { revision: '0' }
     requireRevisionIndex(mockReq, mockRes, mockNext)
     expect(mockRes.status).to.have.been.calledWith(400)
-    expect(mockRes.send).to.have.been.calledWithMatch({ message: '-1 is not a valid index for any revision of this page. Please provide an index between 0 and 0.' })
+    expect(mockRes.send).to.have.been.calledWithMatch({ message: '0 is not a valid index for any revision of this page. Please provide an index between 1 and 1.' })
     expect(mockReq.revision).to.equal(undefined)
     expect(mockNext).to.have.callCount(0)
   })
 
   it('returns 400 if given a number too large to be a revision', async () => {
     mockReq.page = new Page({ revisions })
-    mockReq.params = { revision: '1' }
+    mockReq.params = { revision: '2' }
     requireRevisionIndex(mockReq, mockRes, mockNext)
     expect(mockRes.status).to.have.been.calledWith(400)
-    expect(mockRes.send).to.have.been.calledWithMatch({ message: '1 is not a valid index for any revision of this page. Please provide an index between 0 and 0.' })
+    expect(mockRes.send).to.have.been.calledWithMatch({ message: '2 is not a valid index for any revision of this page. Please provide an index between 1 and 1.' })
     expect(mockReq.revision).to.equal(undefined)
     expect(mockNext).to.have.callCount(0)
   })
 
   it('calls the next function if given a valid revision', async () => {
     mockReq.page = new Page({ revisions })
-    mockReq.params = { revision: '0' }
+    mockReq.params = { revision: '1' }
     requireRevisionIndex(mockReq, mockRes, mockNext)
     expect(mockReq.revision).to.be.an.instanceOf(Revision)
     expect(mockNext).to.have.callCount(1)
--- src/middlewares/require-revision-index.ts
+++ src/middlewares/require-revision-index.ts
@@ -1,14 +1,13 @@
 import { Request, Response, NextFunction } from 'express'
 
 const requireRevisionIndex = (req: Request, res: Response, next: NextFunction): void => {
-  const r = parseInt(req.params.revision)
-  const max = (req.page?.revisions.length ?? 0) - 1
-  if (max < 0) {
+  const revision = req.page?.getRevisionFromStr(req.params.revision)
+  if (revision === undefined) {
     res.status(404).send({ message: 'Page not found.' })
-  } else if (isNaN(r) || r < 0 || r > max) {
-    res.status(400).send({ message: `${req.params.revision} is not a valid index for any revision of this page. Please provide an index between 0 and ${max}.`})
+  } else if (typeof revision === 'string') {
+    res.status(400).send({ message: revision })
   } else {
-    req.revision = req.page?.revisions[r]
+    req.revision = revision
     next()
   }
 }
