2bccc8f5680a126740cc7f83f1a26122d8214f86
Jason Godesky <jefgodesky@gmail.com>
2022-10-05 12:02:32 -0400

    Add new middleware: requireRevisionIndex

---
+++ src/middlewares/require-revision-index.spec.ts
@@ -0,0 +1,72 @@
+import chai, { expect } from 'chai'
+import { mockRequest, mockResponse } from 'mock-req-res'
+import * as sinon from 'sinon'
+import sinonChai from 'sinon-chai'
+import Page from '../models/page/page.js'
+import requireRevisionIndex from './require-revision-index.js'
+
+chai.use(sinonChai)
+
+describe('requireRevisionIndex', () => {
+  let mockReq = mockRequest()
+  let mockRes = mockResponse()
+  let mockNext = (): void => {}
+  const revisions = [{ content: { title: 'First', body: 'First!' } }]
+
+  beforeEach(async () => {
+    mockReq = mockRequest()
+    mockRes = mockResponse()
+    mockNext = sinon.spy()
+  })
+
+  afterEach(() => sinon.restore())
+
+  it('returns 404 if not given a page', async () => {
+    requireRevisionIndex(mockReq, mockRes, mockNext)
+    expect(mockRes.status).to.have.been.calledWith(404)
+    expect(mockRes.send).to.have.been.calledWithMatch({ message: 'Page not found.' })
+    expect(mockNext).to.have.callCount(0)
+  })
+
+  it('returns 400 if not given a revision', async () => {
+    mockReq.page = new Page({ revisions })
+    requireRevisionIndex(mockReq, mockRes, mockNext)
+    expect(mockRes.status).to.have.been.calledWith(400)
+    expect(mockRes.send).to.have.been.calledWithMatch({ message: 'undefined is not a valid index for any revision of this page. Please provide an index between 0 and 0.' })
+    expect(mockNext).to.have.callCount(0)
+  })
+
+  it('returns 400 if given a string for a revision', async () => {
+    mockReq.page = new Page({ revisions })
+    mockReq.params = { revision: 'nope' }
+    requireRevisionIndex(mockReq, mockRes, mockNext)
+    expect(mockRes.status).to.have.been.calledWith(400)
+    expect(mockRes.send).to.have.been.calledWithMatch({ message: 'nope is not a valid index for any revision of this page. Please provide an index between 0 and 0.' })
+    expect(mockNext).to.have.callCount(0)
+  })
+
+  it('returns 400 if given a number less than 0 for a revision', async () => {
+    mockReq.page = new Page({ revisions })
+    mockReq.params = { revision: '-1' }
+    requireRevisionIndex(mockReq, mockRes, mockNext)
+    expect(mockRes.status).to.have.been.calledWith(400)
+    expect(mockRes.send).to.have.been.calledWithMatch({ message: '-1 is not a valid index for any revision of this page. Please provide an index between 0 and 0.' })
+    expect(mockNext).to.have.callCount(0)
+  })
+
+  it('returns 400 if given a number too large to be a revision', async () => {
+    mockReq.page = new Page({ revisions })
+    mockReq.params = { revision: '1' }
+    requireRevisionIndex(mockReq, mockRes, mockNext)
+    expect(mockRes.status).to.have.been.calledWith(400)
+    expect(mockRes.send).to.have.been.calledWithMatch({ message: '1 is not a valid index for any revision of this page. Please provide an index between 0 and 0.' })
+    expect(mockNext).to.have.callCount(0)
+  })
+
+  it('calls the next function if given a valid revision', async () => {
+    mockReq.page = new Page({ revisions })
+    mockReq.params = { revision: '0' }
+    requireRevisionIndex(mockReq, mockRes, mockNext)
+    expect(mockNext).to.have.callCount(1)
+  })
+})
---
+++ src/middlewares/require-revision-index.ts
@@ -0,0 +1,15 @@
+import { Request, Response, NextFunction } from 'express'
+
+const requireRevisionIndex = (req: Request, res: Response, next: NextFunction): void => {
+  const r = parseInt(req.params.revision)
+  const max = (req.page?.revisions.length ?? 0) - 1
+  if (max < 0) {
+    res.status(404).send({ message: 'Page not found.' })
+  } else if (isNaN(r) || r < 0 || r > max) {
+    res.status(400).send({ message: `${req.params.revision} is not a valid index for any revision of this page. Please provide an index between 0 and ${max}.`})
+  } else {
+    next()
+  }
+}
+
+export default requireRevisionIndex
