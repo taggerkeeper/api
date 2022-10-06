ba97fcc29a155002e9bb064e3acb3bc3065e9cda
Jason Godesky <jefgodesky@gmail.com>
2022-10-06 15:28:33 -0400

    Fix linting error

--- src/middlewares/get-revision.ts
+++ src/middlewares/get-revision.ts
@@ -1,7 +1,7 @@
 import { Request, Response, NextFunction } from 'express'
 
 const getRevision = (req: Request, res: Response, next: NextFunction): void => {
-  const revision = req.page?.getRevisionFromStr(req.params.revision as string)
+  const revision = req.page?.getRevisionFromStr(req.params.revision)
   if (typeof revision === 'string') {
     res.status(400).send({ message: revision })
   } else {
