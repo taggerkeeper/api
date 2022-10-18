57f484122dd5c5a5270375cc83a519958c708435
Jason Godesky <jefgodesky@gmail.com>
2022-10-18 11:00:26 -0400

    PublicRevisionData: Add optional file and thumbnail

--- src/models/revision/public.ts
+++ src/models/revision/public.ts
@@ -1,4 +1,5 @@
 import ContentData, { isContentData } from '../content/data.js'
+import FileData, { isFileData } from '../file/data.js'
 import PermissionsData, { isPermissionsData } from '../permissions/data.js'
 import PublicUserData, { isPublicUserData } from '../user/public.js'
 import checkAll from '../../utils/check-all.js'
@@ -7,6 +8,8 @@ import exists from '../../utils/exists.js'
 
 interface PublicRevisionData {
   content: ContentData
+  file?: FileData
+  thumbnail?: FileData
   permissions?: PermissionsData
   editor?: PublicUserData | PublicUserData['id']
   msg?: string
@@ -15,9 +18,11 @@ interface PublicRevisionData {
 
 const isPublicRevisionData = (obj: any): obj is PublicRevisionData => {
   if (!exists(obj) || typeof obj !== 'object') return false
-  const { content, permissions, editor, msg, timestamp } = obj
+  const { content, file, thumbnail, permissions, editor, msg, timestamp } = obj
   return checkAll([
     checkAll([exists(content), isContentData(content)]),
+    checkAny([!exists(file), isFileData(file)]),
+    checkAny([!exists(thumbnail), isFileData(thumbnail)]),
     checkAny([!exists(permissions), isPermissionsData(permissions)]),
     checkAny([!exists(editor), isPublicUserData(editor), typeof editor === 'string', typeof editor === 'object']),
     checkAny([!exists(msg), typeof msg === 'string']),
