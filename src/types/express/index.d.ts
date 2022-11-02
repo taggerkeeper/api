f8ed726ff0cdc9fdb400621eda237a5eb6516163
Jason Godesky <jefgodesky@gmail.com>
2022-11-02 14:28:22 -0400

    Use RevisionsDiff for req.revisionsDiff

--- src/types/express/index.d.ts
+++ src/types/express/index.d.ts
@@ -1,27 +1,14 @@
 import * as sinon from 'sinon'
-import { Change } from 'diff'
 import Email from '../../models/email/email.js'
 import User, { TokenSet } from '../../models/user/user.js'
-import Revision from '../../models/revision/revision.js'
+import Revision, { RevisionsDiff } from '../../models/revision/revision.js'
 import Page from '../../models/page/page.js'
 import { PageQueryResultSet } from '../../models/page/search/data'
-
-interface RevisionDiff {
-  content: {
-    title: Change[]
-    path: Change[]
-    body: Change[]
-  }
-  permissions: {
-    read: Change[]
-    write: Change[]
-  }
-}
 
 declare global {
   namespace Express {
     interface Request {
-      revisionsDiff?: RevisionDiff
+      revisionsDiff?: RevisionsDiff
       email?: Email
       emailer?: Function | sinon.SinonStub
       page?: Page
