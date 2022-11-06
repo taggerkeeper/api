f6bb4d7424017b152027c8b9030659fa1641e322
Jason Godesky <jefgodesky@gmail.com>
2022-11-06 10:55:50 -0500

    Expecter: hasStatusAndHeaders: Headers can be string or regex

--- src/routes/tests/expecters/has-status-and-headers.ts
+++ src/routes/tests/expecters/has-status-and-headers.ts
@@ -1,10 +1,14 @@
 import { expect } from 'chai'
 import { Response } from 'superagent'
 
-const hasStatusAndHeaders = (res: Response, status: number, headers: { [key: string]: string }): void => {
+const hasStatusAndHeaders = (res: Response, status: number, headers: { [key: string]: string | RegExp }): void => {
   expect(res.status).to.equal(status)
   for (const header of Object.keys(headers)) {
-    expect(res.headers[header]).to.equal(headers[header])
+    if (typeof headers[header] === 'string') {
+      expect(res.headers[header]).to.equal(headers[header])
+    } else {
+      expect(res.headers[header]).to.match(headers[header] as RegExp)
+    }
   }
 }
 
