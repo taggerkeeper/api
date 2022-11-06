7789d9e1fce28ec9fc8f58c7ea3a2f1a4e4d2d6c
Jason Godesky <jefgodesky@gmail.com>
2022-11-06 11:24:48 -0500

    Expecter: isFile: Make sure the file can be accessed as expected

--- src/routes/tests/expecters/is-file.ts
+++ src/routes/tests/expecters/is-file.ts
@@ -1,4 +1,5 @@
 import { expect } from 'chai'
+import axios from 'axios'
 
 interface FileExpectations {
   key?: string | RegExp
@@ -13,6 +14,13 @@ const isFile = async (obj: any, expectations: FileExpectations): Promise<void> =
   if (size !== undefined) expect(obj.size).to.equal(size)
   if (mime !== undefined && typeof mime === 'string') expect(obj.mime).to.equal(mime)
   if (mime !== undefined && typeof mime !== 'string') expect(obj.mime).to.match(mime)
+
+  expect(typeof obj.location).to.equal('string')
+  const check = await axios.get(obj.location)
+  expect(check.status).to.equal(200)
+  if (mime !== undefined && typeof mime === 'string') expect(check.headers['content-type']).to.equal(mime)
+  if (mime !== undefined && typeof mime !== 'string') expect(check.headers['content-type']).to.match(mime)
+  expect(check.headers['content-length']).to.equal(size?.toString())
 }
 
 export default isFile
